9a7687fd6c7c28ae57d711dbebf3c6bcae0dd720
Phil Howard <phil@gadgetoid.com>
2021-01-20 15:19:38 +0000

    Add usermod.cmake files for building against upstream usermod micropython usermod branch
    Only *one* usermod directory can be specified, so the build command becomes something like:
    
    make USER_C_MODULES=/path/to/pimoroni-pico/micropython/modules/
    
    The `usermod.cmake` in the "modules" dir will then include all of the modules. Comment lines our here to disable them.
    
    No need to configure anythign in `mpconfigport.h` since the defines are set by the `usermod.cmake` files.

---
+++ micropython/modules/usermod.cmake
@@ -0,0 +1,3 @@
+include(${CMAKE_CURRENT_LIST_DIR}/pico_scroll/usermod.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/pico_rgb_keypad/usermod.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/pico_unicorn/usermod.cmake)
