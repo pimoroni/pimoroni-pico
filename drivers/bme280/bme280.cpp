8ee65ffbdde35d4f0134194e19c35e99b5749f03
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-02-15 14:34:20 +0000

    Added boilerplate driver classes for all current breakouts

---
+++ drivers/bme280/bme280.cpp
@@ -0,0 +1,30 @@
+#include "bme280.hpp"
+
+namespace pimoroni {
+
+    /***** Device registers and masks here *****/
+
+    bool BME280::init() {
+        bool succeeded = false;
+
+        i2c_init(i2c, 400000);
+
+        gpio_set_function(sda, GPIO_FUNC_I2C);
+        gpio_pull_up(sda);
+        gpio_set_function(scl, GPIO_FUNC_I2C);
+        gpio_pull_up(scl);
+
+        if(interrupt != PIN_UNUSED) {
+            gpio_set_function(interrupt, GPIO_FUNC_SIO);
+            gpio_set_dir(interrupt, GPIO_IN);
+            gpio_pull_up(interrupt);
+        }
+
+        /***** Replace if(true) with any operations needed to initialise the device *****/
+        if(true) {
+            succeeded = true;
+        }
+
+        return succeeded;
+    }
+}
---
+++ drivers/sh1107/sh1107.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "hardware/i2c.h"
+#include "hardware/gpio.h"
+
+namespace pimoroni {
+
+    class SH1107 {
+        //--------------------------------------------------
+        // Constants
+        //--------------------------------------------------
+    public:
+        static const uint8_t DEFAULT_I2C_ADDRESS        = 0x0A;
+        static const uint8_t DEFAULT_SDA_PIN            = 20;
+        static const uint8_t DEFAULT_SCL_PIN            = 21;
+        static const uint8_t DEFAULT_INT_PIN            = 22;
+        static const uint8_t PIN_UNUSED                 = UINT8_MAX;
+
+        /***** More public constants here *****/
+
+    private:
+        /***** Private constants here *****/
+
+
+        //--------------------------------------------------
+        // Variables
+        //--------------------------------------------------
+    private:
+        i2c_inst_t *i2c     = i2c0;
+
+        // interface pins with our standard defaults where appropriate
+        int8_t address      = DEFAULT_I2C_ADDRESS;
+        int8_t sda          = DEFAULT_SDA_PIN;
+        int8_t scl          = DEFAULT_SCL_PIN;
+        int8_t interrupt    = DEFAULT_INT_PIN;
+
+        /***** More variables here *****/
+
+
+        //--------------------------------------------------
+        // Constructors/Destructor
+        //--------------------------------------------------
+    public:
+        SH1107() {}
+
+        SH1107(uint8_t address) :
+            address(address) {}
+
+        SH1107(i2c_inst_t *i2c, uint8_t address, uint8_t sda, uint8_t scl, uint8_t interrupt = PIN_UNUSED) :
+            i2c(i2c), address(address), sda(sda), scl(scl), interrupt(interrupt) {}
+
+
+
+        //--------------------------------------------------
+        // Methods
+        //--------------------------------------------------
+    public:
+        bool init(); //This should be present in all drivers
+
+        /***** More public methods here *****/
+
+    private:
+        /***** Private methods here *****/
+    };
+
+}
