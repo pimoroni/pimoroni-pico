31b244a43d60f9fe03490d7129cf4d13077b8dfe
Phil Howard <phil@gadgetoid.com>
2021-02-23 17:36:30 +0000

    Use both fonts simultaneously in text demo

--- examples/pico_explorer/text_demo.cpp
+++ examples/pico_explorer/text_demo.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 
 #include "pico_explorer.hpp"
+#include "font6_data.hpp"
 #include "font8_data.hpp"
 #include "msa301.hpp"
 
@@ -26,7 +27,10 @@ int main() {
     pico_explorer.clear();
 
     pico_explorer.set_pen(255, 255, 255);
-    pico_explorer.text("Hello World. How are you today?", Point(10, 190), 100);
+    pico_explorer.set_font(&font6);
+    pico_explorer.text("6x6: Hello World. How are you today?", Point(10, 10), 220);
+    pico_explorer.set_font(&font8);
+    pico_explorer.text("6x8: Hello World. How are you today?", Point(10, 120), 220);
 
     pico_explorer.update();
 
