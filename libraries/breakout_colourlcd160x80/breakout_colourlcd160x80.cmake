2dac25d09260df63aa5433599f69552c2786c820
Phil Howard <phil@gadgetoid.com>
2021-03-04 10:19:17 +0000

    ST7735 driver and example

--- libraries/breakout_colourlcd160x80/breakout_colourlcd160x80.cmake
+++ libraries/breakout_colourlcd160x80/breakout_colourlcd160x80.cmake
@@ -8,4 +8,4 @@ target_sources(${LIB_NAME} INTERFACE
 target_include_directories(${LIB_NAME} INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
 # Pull in pico libraries that we need
-target_link_libraries(${LIB_NAME} INTERFACE pico_stdlib hardware_spi st7789)
+target_link_libraries(${LIB_NAME} INTERFACE pico_stdlib st7735 pico_graphics hardware_spi)
