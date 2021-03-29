9977b242406acf7e80a68547b7e32ea866e4c394
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-03-29 17:17:48 +0100

    Added commit warning

--- examples/pico_wireless/secrets.h
+++ examples/pico_wireless/secrets.h
@@ -1,4 +1,7 @@
 #include "pico/stdlib.h"
 
+//DO NOT COMMIT THIS FILE!!!
+
+//Your wireless network's name and password
 static const char NETWORK[] = "network name here";
 static const char PASSWORD[] = "network password here";
