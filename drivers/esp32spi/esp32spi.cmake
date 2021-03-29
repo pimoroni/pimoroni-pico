8ff49474eadd8268ba578ec109d4fb3dbca2c3ba
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-03-29 21:08:29 +0100

    Progressed on Wifi class, adding IPAddress

--- drivers/esp32spi/esp32spi.cmake
+++ drivers/esp32spi/esp32spi.cmake
@@ -1,9 +1,10 @@
-set(DRIVER_NAME esp32wireless)
+set(DRIVER_NAME esp32spi)
 add_library(${DRIVER_NAME} INTERFACE)
 
 target_sources(${DRIVER_NAME} INTERFACE
   ${CMAKE_CURRENT_LIST_DIR}/${DRIVER_NAME}.cpp
   ${CMAKE_CURRENT_LIST_DIR}/spi_drv.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/ip_address.cpp
 )
 
 target_include_directories(${DRIVER_NAME} INTERFACE ${CMAKE_CURRENT_LIST_DIR})
