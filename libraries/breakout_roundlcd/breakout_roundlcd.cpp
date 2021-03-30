0866ade05436c669b9f7b40a1b416096ff81d7ca
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-03-30 13:51:00 +0100

    Created dedicated library class for round LCD breakout

---
+++ libraries/breakout_roundlcd/breakout_roundlcd.cpp
@@ -0,0 +1,53 @@
+#include "breakout_roundlcd.hpp"
+
+namespace pimoroni {
+
+  BreakoutRoundLCD::BreakoutRoundLCD(uint16_t *buf)
+    : PicoGraphics(WIDTH, HEIGHT, buf), screen(WIDTH, HEIGHT, buf)  {
+    __fb = buf;
+  }
+
+  BreakoutRoundLCD::BreakoutRoundLCD(uint16_t *buf,  spi_inst_t *spi,
+      uint8_t cs, uint8_t dc, uint8_t sck, uint8_t mosi, uint8_t miso)
+    : PicoGraphics(WIDTH, HEIGHT, buf), screen(WIDTH, HEIGHT, buf, spi, cs, dc, sck, mosi, miso)  {
+    __fb = buf;
+  }
+
+  void BreakoutRoundLCD::init() {
+    // initialise the screen
+    screen.init(true, true);
+  }
+
+  // spi_inst_t* BreakoutRoundLCD::get_spi() const {
+  //   return screen.get_spi();
+  // }
+
+  // int BreakoutRoundLCD::get_cs() const {
+  //   return screen.get_cs();
+  // }
+
+  // int BreakoutRoundLCD::get_dc() const {
+  //   return screen.get_dc();
+  // }
+
+  // int BreakoutRoundLCD::get_sck() const {
+  //   return screen.get_sck();
+  // }
+
+  // int BreakoutRoundLCD::get_mosi() const {
+  //   return screen.get_mosi();
+  // }
+
+  // int BreakoutRoundLCD::get_miso() const {
+  //   return screen.get_miso();
+  // }
+
+  void BreakoutRoundLCD::update() {
+    screen.update();
+  }
+
+  void BreakoutRoundLCD::set_backlight(uint8_t brightness) {
+    screen.set_backlight(brightness);
+  }
+
+}
