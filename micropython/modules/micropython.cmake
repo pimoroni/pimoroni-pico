797f60b0f31baa8109c1f0e34813ed83605bca38
Graeme Winter <graeme.winter@gmail.com>
2021-04-17 13:27:02 +0100

    Restore commented out sections

--- micropython/modules/micropython.cmake
+++ micropython/modules/micropython.cmake
@@ -1,5 +1,7 @@
+include(${CMAKE_CURRENT_LIST_DIR}/breakout_roundlcd/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pico_scroll/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pico_rgb_keypad/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pico_unicorn/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pico_display/micropython.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/pico_explorer/micropython.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/ulab/code/micropython.cmake)
