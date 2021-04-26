b606e31ea140b3d1a96118e091cf1fb9f93daf0d
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-04-26 11:14:00 +0100

    Linting fixes

--- micropython/examples/breakout_trackball/demo.py
+++ micropython/examples/breakout_trackball/demo.py
@@ -5,21 +5,21 @@ sensitivity = 2
 
 trackball = BreakoutTrackball()
 
-trackball.set_rgbw(0, 0, 0, 64);
+trackball.set_rgbw(0, 0, 0, 64)
 
 while True:
     state = trackball.read();
-    if state[BreakoutTrackball.SW_PRESSED] :
-      trackball.set_rgbw(0, 0, 0, 255)
+    if state[BreakoutTrackball.SW_PRESSED]:
+        trackball.set_rgbw(0, 0, 0, 255)
     elif state[BreakoutTrackball.LEFT] > sensitivity:
-      trackball.set_rgbw(0, 0, 255, 0)
+        trackball.set_rgbw(0, 0, 255, 0)
     elif state[BreakoutTrackball.RIGHT] > sensitivity:
-      trackball.set_rgbw(255, 0, 0, 0)
+        trackball.set_rgbw(255, 0, 0, 0)
     elif state[BreakoutTrackball.UP] > sensitivity:
-      trackball.set_rgbw(255, 255, 0, 0)
+        trackball.set_rgbw(255, 255, 0, 0)
     elif state[BreakoutTrackball.DOWN] > sensitivity:
-      trackball.set_rgbw(0, 255, 0, 0)
+        trackball.set_rgbw(0, 255, 0, 0)
     elif state[BreakoutTrackball.SW_CHANGED]:
-      trackball.set_rgbw(0, 0, 0, 64)
-      
+        trackball.set_rgbw(0, 0, 0, 64)
+
     time.sleep(0.02)
