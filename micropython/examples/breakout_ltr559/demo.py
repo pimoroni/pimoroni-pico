d7d6a6e4e35ebbed0c4175689baeae72ce5ab34b
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-05-04 15:49:57 +0100

    Fixed for MP linting

--- micropython/examples/breakout_ltr559/demo.py
+++ micropython/examples/breakout_ltr559/demo.py
@@ -7,12 +7,12 @@ x = 0
 y = 0
 light = True
 
-part_id = ltr.part_id();
-print("Found LTR559. Part ID: 0x", hex(part_id), sep="");
+part_id = ltr.part_id()
+print("Found LTR559. Part ID: 0x", hex(part_id), sep="")
 
 while True:
-    reading = ltr.get_reading();
-    if reading != None:
+    reading = ltr.get_reading()
+    if reading is not None:
         print("Lux:", reading[BreakoutLTR559.LUX], "Prox:", reading[BreakoutLTR559.PROXIMITY])
 
     time.sleep(0.1)
