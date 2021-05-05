18c141170672e4483e6c66ec759294718bbd2007
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-05-05 11:34:21 +0100

    Removed unneeded variables

--- micropython/examples/breakout_ltr559/demo.py
+++ micropython/examples/breakout_ltr559/demo.py
@@ -3,10 +3,6 @@ from breakout_ltr559 import BreakoutLTR559
 
 ltr = BreakoutLTR559()
 
-x = 0
-y = 0
-light = True
-
 part_id = ltr.part_id()
 print("Found LTR559. Part ID: 0x", hex(part_id), sep="")
 
