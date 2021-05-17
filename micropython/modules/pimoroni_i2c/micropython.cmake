b2056040e83747aa870557641a9e66a26512d7f9
Phil Howard <phil@gadgetoid.com>
2021-05-17 18:09:39 +0100

    Port Encoder and Potentiometer to Pimoroni I2C
    Wraps just enough of Pimoroni I2C to make it work in MicroPython.
    
    Ports Encoder and Potentiometer to use a PimorniI2C() instance in lieu of sda/scl.

--- micropython/modules/pimoroni_i2c/micropython.cmake
+++ micropython/modules/pimoroni_i2c/micropython.cmake
@@ -3,6 +3,8 @@ string(TOUPPER ${MOD_NAME} MOD_NAME_UPPER)
 add_library(usermod_${MOD_NAME} INTERFACE)
 
 target_sources(usermod_${MOD_NAME} INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/${MOD_NAME}.c
+    ${CMAKE_CURRENT_LIST_DIR}/${MOD_NAME}.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../../../common/pimoroni_i2c.cpp
 )
 
@@ -11,7 +13,7 @@ target_include_directories(usermod_${MOD_NAME} INTERFACE
 )
 
 target_compile_definitions(usermod_${MOD_NAME} INTERFACE
-    -DMODULE_${MOD_NAME_UPPER}_ENABLED=1
+    MODULE_${MOD_NAME_UPPER}_ENABLED=1
 )
 
 target_link_libraries(usermod INTERFACE usermod_${MOD_NAME})
