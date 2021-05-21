225d5e024f7ca0e1ec6ca1a9fc7e2a6d8a815c08
Phil Howard <phil@gadgetoid.com>
2021-05-21 11:34:52 +0100

    Append CMAKE_MODULE_PATHs to simplify micropython.cmake, drop .mk files
    This change appends the list dir and project root dir to CMAKE_MODULE_PATH so that it doesn't need prepended to each "include" directive.
    
    All .mk files have been deleted, since these are completely redundant.

--- micropython/modules/pico_display/micropython.mk
+++
@@ -1,6 +0,0 @@
-DISPLAY_MOD_DIR := $(USERMOD_DIR)
-# Add all C files to SRC_USERMOD.
-SRC_USERMOD += $(DISPLAY_MOD_DIR)/pico_display.c
-# # We can add our module folder to include paths if needed
-# # This is not actually needed in this example.
-CFLAGS_USERMOD += -I$(DISPLAY_MOD_DIR)
