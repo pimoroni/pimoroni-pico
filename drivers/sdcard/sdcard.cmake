1e5e4c2f94660154ba94ca8c208dd5f5609b1f10
Phil Howard <phil@gadgetoid.com>
2021-08-03 14:13:53 +0100

    Add support for SD over PIO SPI

--- drivers/sdcard/sdcard.cmake
+++ drivers/sdcard/sdcard.cmake
@@ -1,10 +1,13 @@
 if (NOT TARGET sdcard)
     add_library(sdcard INTERFACE)
 
+    pico_generate_pio_header(sdcard ${CMAKE_CURRENT_LIST_DIR}/spi.pio)
+
     target_sources(sdcard INTERFACE
             ${CMAKE_CURRENT_LIST_DIR}/sdcard.c
+            ${CMAKE_CURRENT_LIST_DIR}/pio_spi.c
     )
 
-    target_link_libraries(sdcard INTERFACE fatfs pico_stdlib hardware_clocks hardware_spi)
+    target_link_libraries(sdcard INTERFACE fatfs pico_stdlib hardware_clocks hardware_spi hardware_pio)
     target_include_directories(sdcard INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 endif()
