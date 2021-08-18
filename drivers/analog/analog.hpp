ae0949895354340f6592113ce3bd15d2b1112fcd
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-08-18 12:16:51 +0100

    Added analog class for plasma current reading

---
+++ drivers/analog/analog.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+#include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include "common/pimoroni_common.hpp"
+
+namespace pimoroni {
+
+  class Analog {
+  public:
+    Analog(uint pin, float amplifier_gain = 1.0f, float resistor = 0.0f) :
+    pin(pin), amplifier_gain(amplifier_gain), resistor(resistor) {
+      adc_init();
+
+      //Make sure GPIO is high-impedance, no pullups etc
+      adc_gpio_init(pin);
+
+      //Select ADC input 0 (GPIO26)
+      adc_select_input(pin - 26);
+    };
+    uint16_t read_raw();
+    float read_voltage();
+    float read_current();
+  private:
+    uint pin;
+    float amplifier_gain;
+    float resistor;
+  };
+
+}
