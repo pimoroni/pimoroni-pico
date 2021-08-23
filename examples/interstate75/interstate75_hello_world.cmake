babe8a7760ccc4a05d0d8760c2393e395e841659
Phil Howard <phil@gadgetoid.com>
2021-08-23 22:55:24 +0100

    Pure C++ HUB75 example
    This code is intentionally written in C++ to illustrate the basics of driving HUB75 without being overly complicated to read and understand.
    
    Tested on a 32x32 panel and a 64x64 "FM6126A" panel, for which this code includes a magic pair of register settings.

---
+++ examples/interstate75/interstate75_hello_world.cmake
@@ -0,0 +1,16 @@
+set(OUTPUT_NAME interstate75_hello_world)
+add_executable(${OUTPUT_NAME} interstate75_hello_world.cpp)
+
+# enable usb output
+pico_enable_stdio_usb(${OUTPUT_NAME} 1)
+
+pico_add_extra_outputs(${OUTPUT_NAME})
+
+target_link_libraries(${OUTPUT_NAME}
+    pico_stdlib
+    pico_multicore
+    hardware_pio
+    hardware_dma
+    )
+
+pico_generate_pio_header(${OUTPUT_NAME} ${CMAKE_CURRENT_LIST_DIR}/hub75.pio)
