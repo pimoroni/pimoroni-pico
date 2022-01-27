69e5645700b8acd8a20899fe42d579d8b04d8f62
Niko Kotilainen <niko@Niko-M1-MBP.local>
2022-01-27 14:10:31 +0000

    Badger image update reworked

--- examples/badger2040/badger2040_image.cpp
+++ examples/badger2040/badger2040_image.cpp
@@ -15,50 +15,47 @@ using namespace pimoroni;
 
 Badger2040 badger;
 
-uint32_t time() {
-  absolute_time_t t = get_absolute_time();
-  return to_ms_since_boot(t);
-}
+// uint32_t time() {
+//   absolute_time_t t = get_absolute_time();
+//   return to_ms_since_boot(t);
+// }
 
 int main() {
 
   stdio_init_all();
 
-  sleep_ms(500);
+  sleep_ms(20);
 
   printf("\n\n=======\nbadger2040 starting up\n\n");
 
   badger.init();
 
-  badger.pen(15);
-  badger.clear();
-
-  badger.pen(0);
-  badger.font("sans");
-  badger.text("Press A, B, or C", 15, 65, 1.0f);
-  badger.update();
-
-  while(true) {
-
-    printf("> waiting for a button press..");
-    badger.wait_for_press();
-    printf("done!\n");
-
-    if(badger.pressed(badger.A)) {
-      printf("> A pressed\n");
-      badger.image(shaun);
-    }
-
-    if(badger.pressed(badger.B)) {
-      printf("> B pressed\n");
-      badger.image(paul);
-    }
-
-    if(badger.pressed(badger.C)) {
-      printf("> C pressed\n");
-      badger.image(adam);
-    }
-
-    badger.update();
+  if(badger.pressed(badger.A)) {
+    printf("> A pressed\n");
+    badger.image(shaun);
   }
+
+  else if(badger.pressed(badger.B)) {
+    printf("> B pressed\n");
+    badger.image(paul);
+  }
+
+  else if(badger.pressed(badger.C)) {
+    printf("> C pressed\n");
+    badger.image(adam);
+  }
+
+  else {
+    printf("> No A/B/C key pressed\n");
+    badger.pen(15);
+    badger.clear();
+
+    badger.pen(0);
+    badger.font("sans");
+    badger.text("Press A, B, or C", 15, 65, 1.0f);
+  }
+
+  badger.update();
+  badger.halt();
+
 }
