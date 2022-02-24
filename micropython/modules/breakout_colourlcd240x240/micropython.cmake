6585d6a6701006d0333b92b8147d63853c7e2e24
Phil Howard <phil@gadgetoid.com>
2022-02-24 16:57:59 +0000

    MicroPython: Add missing Pico Graphics to LCD breakouts

--- micropython/modules/breakout_colourlcd240x240/micropython.cmake
+++ micropython/modules/breakout_colourlcd240x240/micropython.cmake
@@ -7,6 +7,8 @@ target_sources(usermod_${MOD_NAME} INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/${MOD_NAME}.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../../../libraries/${MOD_NAME}/${MOD_NAME}.cpp
     ${CMAKE_CURRENT_LIST_DIR}/../../../drivers/st7789/st7789.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../../../libraries/pico_graphics/pico_graphics.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/../../../libraries/pico_graphics/types.cpp
 )
 
 target_include_directories(usermod_${MOD_NAME} INTERFACE
