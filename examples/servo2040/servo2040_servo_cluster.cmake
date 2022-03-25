d4a1f6ed617d24a0f6e76c49e9649d5ce134bdb8
ZodiusInfuser <christopher.parrott2@gmail.com>
2022-03-25 23:44:44 +0000

    Disable uart on all examples to avoid confusing servos

--- examples/servo2040/servo2040_servo_cluster.cmake
+++ examples/servo2040/servo2040_servo_cluster.cmake
@@ -6,7 +6,8 @@ target_link_libraries(${OUTPUT_NAME}
         servo2040
         )
 
-# enable usb output
+# enable usb output, disable uart output (so it doesn't confuse any connected servos)
 pico_enable_stdio_usb(${OUTPUT_NAME} 1)
+pico_enable_stdio_uart(${OUTPUT_NAME} 0)
 
 pico_add_extra_outputs(${OUTPUT_NAME})
--- examples/servo2040/servo2040_simple_easing.cmake
+++ examples/servo2040/servo2040_simple_easing.cmake
@@ -7,7 +7,8 @@ target_link_libraries(${OUTPUT_NAME}
         button
         )
 
-# enable usb output
+# enable usb output, disable uart output (so it doesn't confuse any connected servos)
 pico_enable_stdio_usb(${OUTPUT_NAME} 1)
+pico_enable_stdio_uart(${OUTPUT_NAME} 0)
 
 pico_add_extra_outputs(${OUTPUT_NAME})
