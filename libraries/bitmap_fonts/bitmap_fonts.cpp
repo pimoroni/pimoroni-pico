90ea26249679f94a22d95fe413ed3a4d7b0120d8
Phil Howard <phil@gadgetoid.com>
2022-03-29 17:34:42 +0100

    Bitmap Fonts: Add measure functions.

--- libraries/bitmap_fonts/bitmap_fonts.cpp
+++ libraries/bitmap_fonts/bitmap_fonts.cpp
@@ -1,6 +1,19 @@
 #include "bitmap_fonts.hpp"
 
 namespace bitmap {
+  int32_t measure_character(const font_t *font, const char c, const uint8_t scale) {
+    uint8_t char_index = c - 32;
+    return font->widths[char_index] * scale;
+  }
+
+  int32_t measure_text(const font_t *font, const std::string &t, const uint8_t scale) {
+    int32_t text_width = 0;
+    for(auto c : t) {
+      text_width += measure_character(font, c, scale);
+    }
+    return text_width;
+  }
+
   void character(const font_t *font, rect_func rectangle, const char c, const int32_t x, const int32_t y, const uint8_t scale) {
     uint8_t char_index = c - 32;
 
--- libraries/bitmap_fonts/bitmap_fonts.hpp
+++ libraries/bitmap_fonts/bitmap_fonts.hpp
@@ -14,6 +14,9 @@ namespace bitmap {
 
   typedef std::function<void(int32_t x, int32_t y, int32_t w, int32_t h)> rect_func;
 
+  int32_t measure_character(const font_t *font, const char c, const uint8_t scale);
+  int32_t measure_text(const font_t *font, const std::string &t, const uint8_t scale);
+
   void character(const font_t *font, rect_func rectangle, const char c, const int32_t x, const int32_t y, const uint8_t scale = 2);
   void text(const font_t *font, rect_func rectangle, const std::string &t, const int32_t x, const int32_t y, const int32_t wrap, const uint8_t scale = 2);  
 }
