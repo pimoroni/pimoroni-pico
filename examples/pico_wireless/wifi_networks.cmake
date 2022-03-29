30faa0966df05b35d5f9480a98af97d8fa1a8eb6
Phil Howard <phil@gadgetoid.com>
2022-03-29 10:03:23 +0100

    Pico Wireless: WiFi Network scan example

---
+++ examples/pico_wireless/wifi_networks.cmake
@@ -0,0 +1,14 @@
+add_executable(
+  wireless_networks
+  wifi_networks.cpp
+)
+
+# enable usb output, disable uart output
+pico_enable_stdio_usb(wireless_networks 1)
+pico_enable_stdio_uart(wireless_networks 1)
+
+# Pull in pico libraries that we need
+target_link_libraries(wireless_networks pico_stdlib pico_wireless)
+
+# create map/bin/hex file etc.
+pico_add_extra_outputs(wireless_networks)
