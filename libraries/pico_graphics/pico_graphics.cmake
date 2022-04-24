11c2dedaeae57a0ee5fee76c2ee6ab0855d5628b
ThomasPDye <tompdye@googlemail.com>
2022-04-24 13:45:45 +0100

    added include for bitmap_fonts in pico_graphics

--- libraries/pico_graphics/pico_graphics.cmake
+++ libraries/pico_graphics/pico_graphics.cmake
@@ -1,3 +1,6 @@
+
+include(${CMAKE_CURRENT_LIST_DIR}/../bitmap_fonts/bitmap_fonts.cmake)
+
 add_library(pico_graphics 
     ${CMAKE_CURRENT_LIST_DIR}/types.cpp
     ${CMAKE_CURRENT_LIST_DIR}/pico_graphics.cpp
