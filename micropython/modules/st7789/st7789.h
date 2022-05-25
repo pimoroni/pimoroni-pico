b42425b000b0c50f0c987aa61f0669788943d466
Phil Howard <phil@gadgetoid.com>
2022-05-25 12:25:47 +0100

    Tufty2040: MicroPython bindings for ST7789Parallel.

--- micropython/modules/st7789/st7789.h
+++ micropython/modules/st7789/st7789.h
@@ -8,6 +8,8 @@ extern const mp_obj_type_t GenericST7789_type;
 /***** Extern of Class Methods *****/
 extern void GenericST7789_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind);
 extern mp_obj_t GenericST7789_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args);
+extern mp_obj_t GenericST7789Parallel_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args);
+
 extern mp_obj_t GenericST7789_update(mp_obj_t self_in);
 extern mp_obj_t GenericST7789_set_backlight(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
 
