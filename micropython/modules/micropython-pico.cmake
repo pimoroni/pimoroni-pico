9a494828bca4a40e68d21173436947254f157f31
Phil Howard <phil@gadgetoid.com>
2022-05-27 11:12:44 +0100

    CI: Customised MPY modules per-board

---
+++ micropython/modules/micropython-pico.cmake
@@ -0,0 +1,7 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../../)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
+
+include(micropython-common)
---
+++ micropython/modules/micropython-picolipo_16mb.cmake
@@ -0,0 +1,9 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../../)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
+
+include(micropython-common)
+
+enable_ulab()
