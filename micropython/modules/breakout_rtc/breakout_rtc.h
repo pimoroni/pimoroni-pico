28d555fa4e2e296f8e58adcaaa33daef2bd4089a
Phil Howard <phil@gadgetoid.com>
2022-05-30 15:16:43 +0100

    Breakout RTC: Optimise required kwarg functions to positional.

--- micropython/modules/breakout_rtc/breakout_rtc.h
+++ micropython/modules/breakout_rtc/breakout_rtc.h
@@ -21,13 +21,13 @@ extern mp_obj_t BreakoutRTC_reset(mp_obj_t self_in);
 extern mp_obj_t BreakoutRTC_setup(mp_obj_t self_in);
 
 extern mp_obj_t BreakoutRTC_set_time(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-extern mp_obj_t BreakoutRTC_set_seconds(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-extern mp_obj_t BreakoutRTC_set_minutes(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-extern mp_obj_t BreakoutRTC_set_hours(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-extern mp_obj_t BreakoutRTC_set_weekday(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-extern mp_obj_t BreakoutRTC_set_date(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-extern mp_obj_t BreakoutRTC_set_month(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-extern mp_obj_t BreakoutRTC_set_year(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
+extern mp_obj_t BreakoutRTC_set_seconds(mp_obj_t self_in, mp_obj_t sec_in);
+extern mp_obj_t BreakoutRTC_set_minutes(mp_obj_t self_in, mp_obj_t min_in);
+extern mp_obj_t BreakoutRTC_set_hours(mp_obj_t self_in, mp_obj_t hour_in);
+extern mp_obj_t BreakoutRTC_set_weekday(mp_obj_t self_in, mp_obj_t weekday_in);
+extern mp_obj_t BreakoutRTC_set_date(mp_obj_t self_in, mp_obj_t date_in);
+extern mp_obj_t BreakoutRTC_set_month(mp_obj_t self_in, mp_obj_t month_in);
+extern mp_obj_t BreakoutRTC_set_year(mp_obj_t self_in, mp_obj_t year_in);
 extern mp_obj_t BreakoutRTC_set_to_compiler_time(mp_obj_t self_in);
 
 extern mp_obj_t BreakoutRTC_update_time(mp_obj_t self_in);
@@ -48,7 +48,7 @@ extern mp_obj_t BreakoutRTC_is_pm(mp_obj_t self_in);
 extern mp_obj_t BreakoutRTC_set_12_hour(mp_obj_t self_in);
 extern mp_obj_t BreakoutRTC_set_24_hour(mp_obj_t self_in);
 
-extern mp_obj_t BreakoutRTC_set_unix(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
+extern mp_obj_t BreakoutRTC_set_unix(mp_obj_t self_in, mp_obj_t value_in);
 extern mp_obj_t BreakoutRTC_get_unix(mp_obj_t self_in);
 
 extern mp_obj_t BreakoutRTC_enable_alarm_interrupt(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
@@ -72,10 +72,10 @@ extern mp_obj_t BreakoutRTC_clear_periodic_update_interrupt_flag(mp_obj_t self_i
 
 extern mp_obj_t BreakoutRTC_enable_trickle_charge(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
 extern mp_obj_t BreakoutRTC_disable_trickle_charge(mp_obj_t self_in);
-extern mp_obj_t BreakoutRTC_set_backup_switchover_mode(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
+extern mp_obj_t BreakoutRTC_set_backup_switchover_mode(mp_obj_t self_in, mp_obj_t val_in);
 
-extern mp_obj_t BreakoutRTC_enable_clock_out(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
-extern mp_obj_t BreakoutRTC_enable_interrupt_controlled_clockout(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
+extern mp_obj_t BreakoutRTC_enable_clock_out(mp_obj_t self_in, mp_obj_t freq_in);
+extern mp_obj_t BreakoutRTC_enable_interrupt_controlled_clockout(mp_obj_t self_in, mp_obj_t freq_in);
 extern mp_obj_t BreakoutRTC_disable_clock_out(mp_obj_t self_in);
 extern mp_obj_t BreakoutRTC_read_clock_output_interrupt_flag(mp_obj_t self_in);
 extern mp_obj_t BreakoutRTC_clear_clock_output_interrupt_flag(mp_obj_t self_in);
