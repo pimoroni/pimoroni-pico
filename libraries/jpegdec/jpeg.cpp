bb2db8423e025464bb8c8cbafdb080fa0463bf3d
Phil Howard <phil@gadgetoid.com>
2022-06-24 11:13:03 +0100

    JPEGDEC: Add CMake files.

---
+++ libraries/jpegdec/jpeg.cpp
@@ -0,0 +1,16 @@
+#include "JPEGDEC.h"
+/*
+int32_t readRAM(JPEGFILE *pFile, uint8_t *pBuf, int32_t iLen) {
+  return 0;
+}
+int32_t seekMem(JPEGFILE *pFile, int32_t iPosition) {
+  return 0;
+}
+int32_t readFile(JPEGFILE *pFile, uint8_t *pBuf, int32_t iLen) {
+  return 0;
+}
+int32_t seekFile(JPEGFILE *pFile, int32_t iPosition) {
+  return 0;
+}
+void closeFile(void *handle) {
+}*/
