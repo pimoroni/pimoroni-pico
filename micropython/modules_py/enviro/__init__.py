2a9587d1ad1788366c553c8eb9a700d667798bbe
ZodiusInfuser <christopher.parrott2@gmail.com>
2022-07-05 15:25:27 +0100

    Fix for library not getting copied over to frozen

--- micropython/modules_py/enviro/__init__.py
+++ micropython/modules_py/enviro/__init__.py
@@ -1,5 +1,5 @@
 # from enviro.camera import EnviroCamera
-from enviro.grow import EnviroGrow
-from enviro.indoor import EnviroIndoor
-from enviro.urban import EnviroUrban
-from enviro.weather import EnviroWeather
+from enviro.grow import *  # noqa F401
+from enviro.indoor import *  # noqa F401
+from enviro.urban import *  # noqa F401
+from enviro.weather import *  # noqa F401
--- micropython/modules_py/modules_py.cmake
+++ micropython/modules_py/modules_py.cmake
@@ -1,14 +1,16 @@
-function (copy_module TARGET SRC DST)
+function (copy_module TARGET SRC DIR DST)
     add_custom_command(
-        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/../modules/${DST}.py
+        OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/../modules/${DIR}/${DST}.py
 
         COMMAND
-            cp ${SRC} ${CMAKE_CURRENT_BINARY_DIR}/../modules/${DST}.py
+            mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/../modules/${DIR}
+        COMMAND
+            cp ${SRC} ${CMAKE_CURRENT_BINARY_DIR}/../modules/${DIR}/${DST}.py
 
         DEPENDS ${src}
     )
 
-    target_sources(${TARGET} INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/../modules/${DST}.py)
+    target_sources(${TARGET} INTERFACE ${CMAKE_CURRENT_BINARY_DIR}/../modules/${DIR}/${DST}.py)
 endfunction()
 
 # Create a dummy usermod to hang our .py copies from
@@ -16,17 +18,17 @@ add_library(usermod_modules_py INTERFACE)
 target_link_libraries(usermod INTERFACE usermod_modules_py)
 
 # .py files to copy from modules_py to ports/rp2/modules
-#copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/picosystem.py picosystem)
-copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/pimoroni.py pimoroni)
+#copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/picosystem.py . picosystem)
+copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/pimoroni.py . pimoroni)
 
 if(PICO_BOARD STREQUAL "pico_w")
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/automation.py automation)
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/inventor.py inventor)
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/__init__.py enviro/__init__)
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/base.py enviro/base)
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/camera.py enviro/camera)
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/grow.py enviro/grow)
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/indoor.py enviro/indoor)
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/urban.py enviro/urban)
-    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/weather.py enviro/weather)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/automation.py . automation)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/inventor.py . inventor)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/__init__.py enviro __init__)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/base.py enviro base)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/camera.py enviro camera)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/grow.py enviro grow)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/indoor.py enviro indoor)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/urban.py enviro urban)
+    copy_module(usermod_modules_py ${CMAKE_CURRENT_LIST_DIR}/enviro/weather.py enviro weather)
 endif()
