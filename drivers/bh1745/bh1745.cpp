10941a7a5023dec5b2f19bcc3f46f38164e1a12e
Phil Howard <phil@gadgetoid.com>
2022-07-08 14:46:54 +0100

    BH1745: Correct CONTROL3 init value.

--- drivers/bh1745/bh1745.cpp
+++ drivers/bh1745/bh1745.cpp
@@ -13,7 +13,7 @@ namespace pimoroni {
         i2c->clear_bits(address, BH1745_REG_SYSTEM_CONTROL, 6);    // Clear INT reset bit
         set_measurement_time_ms(640);
         i2c->set_bits(address, BH1745_REG_MODE_CONTROL2, 4);       // Enable RGBC
-        i2c->set_bits(address, BH1745_REG_MODE_CONTROL3, 0, 0xff); // Turn on sensor
+        i2c->set_bits(address, BH1745_REG_MODE_CONTROL3, 0, 0x02); // Turn on sensor
         set_threshold_high(0x0000);                                // Set threshold so int will always fire
         set_threshold_low(0xFFFF);                                 // this lets us turn on the LEDs with the int pin
         i2c->clear_bits(address, BH1745_REG_INTERRUPT, 4);         // Enable interrupt latch
