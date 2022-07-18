d81151a1c65e0789270d144e0130f9d4b2b048d3
Phil Howard <phil@pimoroni.com>
2022-07-18 15:32:56 +0100

    Inky Frame: Sensible limit on SDCard speed.
    Going any faster will result in corruption- this showed up as visible lines of distortion down decoded JPEG files.

--- drivers/sdcard/sdcard.c
+++ drivers/sdcard/sdcard.c
@@ -51,7 +51,7 @@
 #define CT_BLOCK       0x08            /* Block addressing */
 
 #define CLK_SLOW	(100 * KHZ)
-#define CLK_FAST	(50 * MHZ)
+#define CLK_FAST	(30 * MHZ)
 
 static volatile
 DSTATUS Stat = STA_NOINIT;	/* Physical drive status */
