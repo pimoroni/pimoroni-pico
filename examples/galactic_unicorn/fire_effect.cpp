a3c31a8a8ed4939ab16f7de1ab6a6e8df8b6255d
jon <jon@pimoroni.com>
2022-07-26 04:42:17 +0100

    Playing basic audio samples now works, added rainbow example for photoshoot

--- examples/galactic_unicorn/fire_effect.cpp
+++ examples/galactic_unicorn/fire_effect.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 #include "pico/stdlib.h"
 
 #include "libraries/pico_graphics/pico_graphics.hpp"
@@ -13,13 +14,27 @@ PicoGraphics_PenRGB565 graphics(53, 11, nullptr);
 GalacticUnicorn galactic_unicorn;
 
 // extra row of pixels for sourcing flames and averaging
-float heat[53][15] = {0.0f};
+int width = 53;
+int height = 15;
+
+// a buffer that's at least big enough to store 55 x 15 values (to allow for both orientations)
+float heat[1000] = {0.0f};
+
+void set(int x, int y, float v) {
+  heat[x + y * width] = v;
+}
+
+float get(int x, int y) {
+  return heat[x + y * width];
+}
 
 int main() {
 
   stdio_init_all();
 
   galactic_unicorn.init();
+
+  bool landscape = true;
 
   while(true) {
     if(galactic_unicorn.is_pressed(galactic_unicorn.SWITCH_BRIGHTNESS_UP)) {
@@ -29,59 +44,77 @@ int main() {
       galactic_unicorn.adjust_brightness(-0.01);
     }
 
-    graphics.set_pen(0, 0, 0);
-    graphics.clear();
-
+    if(galactic_unicorn.is_pressed(galactic_unicorn.SWITCH_A)) {
+      landscape = true;
+      width = 53;
+      height = 15;
+      memset(heat, 0, sizeof(heat));
+    }
+    if(galactic_unicorn.is_pressed(galactic_unicorn.SWITCH_B)) {
+      landscape = false;
+      width = 11;
+      height = 55;
+      memset(heat, 0, sizeof(heat));
+    }
     
-    for(int y = 0; y < 14; y++) {
-      for(int x = 0; x < 53; x++) {
-        if(heat[x][y] > 0.5f) {
+    for(int y = 0; y < height; y++) {
+      for(int x = 0; x < width; x++) {
+        float value = get(x, y);
+
+        graphics.set_pen(0, 0, 0);
+        if(value > 0.5f) {
           graphics.set_pen(255, 255, 180);
-          graphics.pixel(Point(x, y));
-        }else if(heat[x][y] > 0.4f) {
+        }else if(value > 0.4f) {
           graphics.set_pen(220, 160, 0);
-          graphics.pixel(Point(x, y));
-        }else if(heat[x][y] > 0.3f) {
+        }else if(value > 0.3f) {
           graphics.set_pen(180, 50, 0);
-          graphics.pixel(Point(x, y));
-        }else if(heat[x][y] > 0.2f) {
+        }else if(value > 0.2f) {
           graphics.set_pen(40, 40, 40);
-          graphics.pixel(Point(x, y));
         }
+        
+        if(landscape) {
+          graphics.pixel(Point(x, y));  
+        }else{
+          graphics.pixel(Point(y, x));  
+        }
+        
 
         // update this pixel by averaging the below pixels
+        float average = 0.0f;
         if(x == 0) {
-          heat[x][y] = (heat[x][y] + heat[x][y + 2] + heat[x][y + 1] + heat[x + 1][y + 1]) / 4.0f;
+          average = (get(x, y) + get(x, y + 2) + get(x, y + 1) + get(x + 1, y + 1)) / 4.0f;
         } else if(x == 52) {
-          heat[x][y] = (heat[x][y] + heat[x][y + 2] + heat[x][y + 1] + heat[x - 1][y + 1]) / 4.0f;
+          average = (get(x, y) + get(x, y + 2) + get(x, y + 1) + get(x - 1, y + 1)) / 4.0f;
         } else {
-          heat[x][y] = (heat[x][y] + heat[x][y + 2] + heat[x][y + 1] + heat[x - 1][y + 1] + heat[x + 1][y + 1]) / 5.0f;
-        } 
+          average = (get(x, y) + get(x, y + 2) + get(x, y + 1) + get(x - 1, y + 1) + get(x + 1, y + 1)) / 5.0f;
+        }
 
-        heat[x][y] -= 0.01f;
-        heat[x][y] = heat[x][y] < 0.0f ? 0.0f: heat[x][y];
+        average -= landscape ? 0.01f : 0.004f;
+        average = average < 0.0f ? 0.0f: average;
+        set(x, y, average);
       }
     }
 
     galactic_unicorn.update(graphics);
 
     // clear the bottom row and then add a new fire seed to it
-    for(int x = 0; x < 53; x++) {
-      heat[x][14] = 0.0f;
+    for(int x = 0; x < width; x++) {
+      set(x, height - 1, 0.0f);
     }
 
     // add a new random heat source
-    for(int c = 0; c < 5; c++) {
-      int px = (rand() % 51) + 1;
-      heat[px][13] = 1.0f;
-      heat[px + 1][13] = 1.0f;
-      heat[px - 1][13] = 1.0f;
-      heat[px][14] = 1.0f;
-      heat[px + 1][14] = 1.0f;
-      heat[px - 1][14] = 1.0f;
+    int source_count = landscape ? 5 : 1;
+    for(int c = 0; c < source_count; c++) {
+      int px = (rand() % (width - 4)) + 2;
+      set(px    , height - 2, 1.0f);
+      set(px + 1, height - 2, 1.0f);
+      set(px - 1, height - 2, 1.0f);
+      set(px    , height - 1, 1.0f);
+      set(px + 1, height - 1, 1.0f);
+      set(px - 1, height - 1, 1.0f);
     }
-    
-    sleep_ms(50);
+
+    sleep_ms(20);
   }
 
   return 0;
