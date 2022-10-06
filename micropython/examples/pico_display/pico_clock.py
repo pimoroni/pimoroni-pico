647346ae60255dfc2959b2cd2e961695dadedbeb
Richard <36215881+hoganri@users.noreply.github.com>
2022-10-06 08:51:23 -0400

    Fix linting errors, handle time as integers
    Fixes linting errors and removes unnecessary type conversions. Time adjustments also moved into individual functions.

--- micropython/examples/pico_display/pico_clock.py
+++ micropython/examples/pico_display/pico_clock.py
@@ -1,79 +1,85 @@
 import time
 from pimoroni import Button
-from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY, PEN_P4
-
-# We're only using a few colours so we can use a 4 bit/16 colour palette and save RAM!
+from picographics import PicoGraphics, DISPLAY_PICO_DISPLAY
 display = PicoGraphics(display=DISPLAY_PICO_DISPLAY, rotate=0)
 display.set_backlight(1.0)
-
 
 button_a = Button(12)
 button_b = Button(13)
 button_x = Button(14)
 button_y = Button(15)
 
+WIDTH, HEIGHT = display.get_bounds()
 WHITE = display.create_pen(255, 255, 255)
 BLACK = display.create_pen(0, 0, 0)
+
 
 def clear():
     display.set_pen(BLACK)
     display.clear()
     display.update()
 
+
 clear()
 
-WIDTH, HEIGHT = display.get_bounds()
-
-currentTimeHours = "12"
-currentTimeMinutes = "00"
 
 lastAdjustment = None
+currentTimeHours = 12
+currentTimeMinutes = 0
+
+
+def incrementHours():
+    global currentTimeHours
+    currentTimeHours += 1
+    if currentTimeHours > 12:
+        currentTimeHours = 1
+
+
+def decrementHours():
+    global currentTimeHours
+    currentTimeHours -= 1
+    if currentTimeHours < 1:
+        currentTimeHours = 12
+
+
+def incrementMinutes():
+    global currentTimeMinutes
+    currentTimeMinutes += 1
+    if currentTimeMinutes > 59:
+        currentTimeMinutes = 0
+
+
+def decrementMinutes():
+    global currentTimeMinutes
+    currentTimeMinutes -= 1
+    if currentTimeMinutes < 1:
+        currentTimeMinutes = 59
+
 
 def showTime():
     clear()
     display.set_pen(WHITE)
-    display.text(str(currentTimeHours)+":"+str(currentTimeMinutes), int(WIDTH/3.5), int(HEIGHT/3), 240, 4)
+    display.text(
+        "{:02}:{:02}".format(
+            currentTimeHours,
+            currentTimeMinutes), int(WIDTH/3.5), int(HEIGHT/3), 240, 4)
     display.update()
+
 
 while True:
     showTime()
-    
-    if button_a.read(): # Handle hours adjustment
-        currentTimeHours = str(int(currentTimeHours)+1)
-        if int(currentTimeHours) < 10:
-            currentTimeHours = "0"+currentTimeHours
-        elif int(currentTimeHours) > 12:
-            currentTimeHours = "01"
+    if button_a.read():  # Handle hours adjustment
+        incrementHours()
     elif button_b.read():
-        currentTimeHours = str(int(currentTimeHours)-1)
-        if int(currentTimeHours) < 1:
-            currentTimeHours = "12"
-        if int(currentTimeHours) < 10:
-            currentTimeHours = "0"+currentTimeHours
-    elif button_x.read(): # Handle minutes adjustment
-        currentTimeMinutes = str(int(currentTimeMinutes)+1)
-        if int(currentTimeMinutes) < 10:
-            currentTimeMinutes = "0"+currentTimeMinutes
-        elif int(currentTimeMinutes) > 59:
-            currentTimeMinutes = "01"
+        decrementHours()
+    elif button_x.read():  # Handle minutes adjustment
+        incrementMinutes()
     elif button_y.read():
-        currentTimeMinutes = str(int(currentTimeMinutes)-1)
-        if int(currentTimeMinutes) < 1:
-            currentTimeMinutes = "59"
-        if int(currentTimeMinutes) < 10:
-            currentTimeMinutes = "0"+currentTimeMinutes
-    
+        decrementMinutes()
     if lastAdjustment is None or time.time()-lastAdjustment >= 59:
-        if currentTimeMinutes != "59":
-            currentTimeMinutes = str(int(currentTimeMinutes)+1)
-            if int(currentTimeMinutes) < 10:
-                currentTimeMinutes = "0"+currentTimeMinutes
+        if currentTimeMinutes != 59:
+            incrementMinutes()
         else:
-            currentTimeMinutes = "00"
-            if currentTimeHours == "12":
-                currentTimeHours = "01"
-            else:
-                currentTimeHours = str(int(currentTimeHours)+1)
-                if int(currentTimeHours) < 10:
-                    currentTimeHours = "0"+currentTimeHours
+            incrementMinutes()
+            incrementHours()
         lastAdjustment = time.time()
