17ef4c2aae9111b193822c5fdb438846995746cc
helgibbons <50950368+helgibbons@users.noreply.github.com>
2022-11-17 16:55:00 +0000

    minor tidying

--- micropython/examples/gfx_pack/calc.py
+++ micropython/examples/gfx_pack/calc.py
@@ -1,17 +1,18 @@
 '''
 calc.py
 Graphical demo for the GFX Pack with Raspberry Pi Pico
-This example draws a nice sine wave on the display, reminiscent of the graphical calculators from the 90s.
+This example draws a nice sine wave on the display,
+reminiscent of the graphical calculators from the 90s.
 '''
+
 from math import radians, sin
 from gfx_pack import GfxPack
-
 
 gp = GfxPack()
 display = gp.display
 WIDTH, HEIGHT = display.get_bounds()
-display.set_backlight(1)  # Turns on the white component of the backlight
-gp.set_backlight(0, 0, 0)  # Leaves The other back light colours off
+display.set_backlight(1.0)  # Turns on the white component of the backlight
+gp.set_backlight(0, 0, 0)  # Leaves the other backlight colours off
 
 
 def draw_axis():
@@ -37,14 +38,14 @@ def draw_text():
     display.text("y=sin(x)           x=(-360:360)", 0, 58, WIDTH, 1)
 
 
-# Clear Display
+# Clear display
 display.set_pen(0)
 display.clear()
 
 # Set pen to black
 display.set_pen(15)
 
-# Draw the details in the picographics frame buffer
+# Draw the details in the PicoGraphics frame buffer
 draw_axis()
 draw_sine(0, True)
 draw_text()
