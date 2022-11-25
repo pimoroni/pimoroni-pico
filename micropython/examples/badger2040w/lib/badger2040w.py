09984014febdfe58984b3c87e8883d110a90f2d3
Phil Howard <phil@pimoroni.com>
2022-11-25 16:48:03 +0000

    Badger2040W: Add examples and libs.

---
+++ micropython/examples/badger2040w/lib/badger2040w.py
@@ -0,0 +1,133 @@
+import machine
+from picographics import PicoGraphics, DISPLAY_INKY_PACK
+from network_manager import NetworkManager
+import WIFI_CONFIG
+import uasyncio
+import gc
+import wakeup
+
+
+BUTTON_UP = 15
+BUTTON_DOWN = 11
+BUTTON_A = 12
+BUTTON_B = 13
+BUTTON_C = 14
+BUTTON_USER = 23
+
+BUTTON_MASK = 0b11111 << 11
+
+SYSTEM_VERY_SLOW = 0
+SYSTEM_SLOW = 1
+SYSTEM_NORMAL = 2
+SYSTEM_FAST = 3
+SYSTEM_TURBO = 4
+
+UPDATE_NORMAL = 0
+UPDATE_MEDIUM = 1
+UPDATE_FAST = 2
+UPDATE_TURBO = 3
+
+WIDTH = 296
+HEIGHT = 128
+
+SYSTEM_FREQS = [
+    4000000,
+    12000000,
+    48000000,
+    133000000,
+    250000000
+]
+
+WAKEUP_GPIO_STATE = wakeup.get_gpio_state()
+
+BUTTONS = {
+    11: machine.Pin(11),
+    12: machine.Pin(12),
+    13: machine.Pin(13),
+    14: machine.Pin(14),
+    15: machine.Pin(15),
+    23: machine.Pin(23)
+}
+
+
+def woken_by_button():
+    return WAKEUP_GPIO_STATE & BUTTON_MASK > 0
+
+
+def pressed_to_wake(button):
+    return WAKEUP_GPIO_STATE & (1 << button) > 0
+
+
+def system_speed(speed):
+    try:
+        machine.freq(SYSTEM_FREQS[speed])
+    except IndexError:
+        pass
+
+
+class Badger2040W():
+    def __init__(self):
+        self.display = PicoGraphics(DISPLAY_INKY_PACK)
+
+    def __getattr__(self, item):
+        # Glue to redirect calls to PicoGraphics
+        if item in dir(self.display):
+            return getattr(self.display, item)
+        elif item in self.__dict__.keys():
+            return getattr(self, item)
+        else:
+            raise AttributeError(f"No attribute '{item}'")
+
+    def led(self, brightness):
+        pass
+
+    def invert(self, invert):
+        pass
+
+    def thickness(self, thickness):
+        print("Thickness!")
+    
+    def halt(self):
+        pass
+        
+    def pressed(self, button):
+        return BUTTONS[button].value() == 1
+
+    @micropython.native
+    def icon(self, data, index, data_w, icon_size, x, y):
+        s_x = (index * icon_size) % data_w
+        s_y = int((index * icon_size) / data_w)
+    
+        for o_y in range(icon_size):
+            for o_x in range(icon_size):
+                o = ((o_y + s_y) * data_w) + (o_x + s_x)
+                bm = 0b10000000 >> (o & 0b111)
+                if data[o >> 3] & bm:
+                    self.display.pixel(x + o_x, y + o_y)
+    
+    def image(self, data, w, h, x, y):
+        for oy in range(h):
+            row = data[oy]
+            for ox in range(w):
+                if row & 0b1 == 0:
+                    self.display.pixel(x + ox, y + oy)
+                row >>= 1
+                
+
+    def status_handler(self, mode, status, ip):
+        print(mode, status, ip)
+        self.display.set_pen(15)
+        self.display.clear()
+        self.display.set_pen(0)
+        if status:
+            self.display.text("Connected!", 10, 10, 300, 0.5)
+            self.display.text(ip, 10, 30, 300, 0.5)
+        else:
+            self.display.text("Connecting...", 10, 10, 300, 0.5)
+        self.display.update()
+
+    def connect(self):
+        self.display.set_update_speed(2)
+        network_manager = NetworkManager(WIFI_CONFIG.COUNTRY, status_handler=self.status_handler)
+        uasyncio.get_event_loop().run_until_complete(network_manager.client(WIFI_CONFIG.SSID, WIFI_CONFIG.PSK))
+        gc.collect()
---
+++ micropython/examples/badger2040w/lib/network_manager.py
@@ -0,0 +1,107 @@
+import rp2
+import network
+import machine
+import uasyncio
+
+
+class NetworkManager:
+    _ifname = ("Client", "Access Point")
+
+    def __init__(self, country="GB", client_timeout=30, access_point_timeout=5, status_handler=None, error_handler=None):
+        rp2.country(country)
+        self._ap_if = network.WLAN(network.AP_IF)
+        self._sta_if = network.WLAN(network.STA_IF)
+
+        self._mode = network.STA_IF
+        self._client_timeout = client_timeout
+        self._access_point_timeout = access_point_timeout
+        self._status_handler = status_handler
+        self._error_handler = error_handler
+        self.UID = ("{:02X}" * 8).format(*machine.unique_id())
+
+    def isconnected(self):
+        return self._sta_if.isconnected() or self._ap_if.isconnected()
+
+    def config(self, var):
+        if self._sta_if.active():
+            return self._sta_if.config(var)
+        else:
+            if var == "password":
+                return self.UID
+            return self._ap_if.config(var)
+
+    def mode(self):
+        if self._sta_if.isconnected():
+            return self._ifname[0]
+        if self._ap_if.isconnected():
+            return self._ifname[1]
+        return None
+
+    def ifaddress(self):
+        if self._sta_if.isconnected():
+            return self._sta_if.ifconfig()[0]
+        if self._ap_if.isconnected():
+            return self._ap_if.ifconfig()[0]
+        return '0.0.0.0'
+
+    def disconnect(self):
+        if self._sta_if.isconnected():
+            self._sta_if.disconnect()
+        if self._ap_if.isconnected():
+            self._ap_if.disconnect()
+
+    async def wait(self, mode):
+        while not self.isconnected():
+            self._handle_status(mode, None)
+            await uasyncio.sleep_ms(1000)
+
+    def _handle_status(self, mode, status):
+        if callable(self._status_handler):
+            self._status_handler(self._ifname[mode], status, self.ifaddress())
+
+    def _handle_error(self, mode, msg):
+        if callable(self._error_handler):
+            if self._error_handler(self._ifname[mode], msg):
+                return
+        raise RuntimeError(msg)
+
+    async def client(self, ssid, psk):
+        if self._sta_if.isconnected():
+            self._handle_status(network.STA_IF, True)
+            return
+
+        self._ap_if.disconnect()
+        self._ap_if.active(False)
+
+        self._sta_if.active(True)
+        self._sta_if.connect(ssid, psk)
+
+        try:
+            await uasyncio.wait_for(self.wait(network.STA_IF), self._client_timeout)
+            self._handle_status(network.STA_IF, True)
+
+        except uasyncio.TimeoutError:
+            self._sta_if.active(False)
+            self._handle_status(network.STA_IF, False)
+            self._handle_error(network.STA_IF, "WIFI Client Failed")
+
+    async def access_point(self):
+        if self._ap_if.isconnected():
+            self._handle_status(network.AP_IF, True)
+            return
+
+        self._sta_if.disconnect()
+        self._sta_if.active(False)
+
+        self._ap_if.ifconfig(("10.10.1.1", "255.255.255.0", "10.10.1.1", "10.10.1.1"))
+        self._ap_if.config(password=self.UID)
+        self._ap_if.active(True)
+
+        try:
+            await uasyncio.wait_for(self.wait(network.AP_IF), self._access_point_timeout)
+            self._handle_status(network.AP_IF, True)
+
+        except uasyncio.TimeoutError:
+            self._sta_if.active(False)
+            self._handle_status(network.AP_IF, False)
+            self._handle_error(network.AP_IF, "WIFI Client Failed")
