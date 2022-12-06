037d9ab03978f1f4bff6a62298d0d23504203968
Gee Bartlett <geeus81@gmail.com>
2022-12-06 16:56:36 +0000

    make a start in the e2802 driver

---
+++ drivers/e2802/e2802.cpp
@@ -0,0 +1,216 @@
+#include "e2802.hpp"
+
+#include <cstdlib>
+#include <math.h>
+#include <string.h>
+
+namespace pimoroni {
+
+  enum reg {
+    PSR = 0x00,
+    PWR = 0x01,
+    POF = 0x02,
+    POFS = 0x03,
+    PON = 0x04,
+    BTST1 = 0x05,
+    BTST2 = 0x06,
+    DSLP = 0x07,
+    BTST3 = 0x08,
+    DTM = 0x10,
+    DSP = 0x11,
+    DRF = 0x12,
+    IPC = 0x13,
+    PLL = 0x30,
+    TSC = 0x40,
+    TSE = 0x41,
+    TSW = 0x42,
+    TSR = 0x43,
+    CDI = 0x50,
+    LPD = 0x51,
+    TCON = 0x60,
+    TRES = 0x61,
+    DAM = 0x65,
+    REV = 0x70,
+    FLG = 0x71,
+    AMV = 0x80,
+    VV = 0x81,
+    VDCS = 0x82,
+    T_VDCS = 0x84,
+    AGID = 0x86,
+    CMDH = 0xAA,
+    CCSET = 0xE0,
+    PWS = 0xE3,
+    TSSET = 0xE5
+  };
+
+  bool E2802::is_busy() {
+    if(BUSY == PIN_UNUSED) {
+      if(absolute_time_diff_us(get_absolute_time(), timeout) > 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return !gpio_get(BUSY);
+  }
+
+  void E2802::busy_wait(uint minimum_wait_ms) {
+    timeout = make_timeout_time_ms(minimum_wait_ms);
+    while(is_busy()) {
+      tight_loop_contents();
+    }
+  }
+
+  void E2802::reset() {
+    gpio_put(RESET, 0); sleep_ms(10);
+    gpio_put(RESET, 1); sleep_ms(10);
+    busy_wait();
+  }
+
+  void E2802::init() {
+    // configure spi interface and pins
+    spi_init(spi, 3'000'000);
+
+    gpio_set_function(DC, GPIO_FUNC_SIO);
+    gpio_set_dir(DC, GPIO_OUT);
+
+    gpio_set_function(CS, GPIO_FUNC_SIO);
+    gpio_set_dir(CS, GPIO_OUT);
+    gpio_put(CS, 1);
+
+    gpio_set_function(RESET, GPIO_FUNC_SIO);
+    gpio_set_dir(RESET, GPIO_OUT);
+    gpio_put(RESET, 1);
+
+    gpio_set_function(BUSY, GPIO_FUNC_SIO);
+    gpio_set_dir(BUSY, GPIO_IN);
+    gpio_set_pulls(BUSY, true, false);
+
+    gpio_set_function(SCK,  GPIO_FUNC_SPI);
+    gpio_set_function(MOSI, GPIO_FUNC_SPI);
+  };
+
+  void E2802::setup() {
+    reset();
+    busy_wait();
+
+    uint8_t dimensions[4] = {
+      uint8_t(width >> 8),
+      uint8_t(width),
+      uint8_t(height >> 8),
+      uint8_t(height)
+    };
+
+    if (width == 600) {
+      if (rotation == ROTATE_0) {
+        command(PSR,  {0xE3, 0x08});
+      } else {
+        command(PSR,  {0xEF, 0x08});
+      }
+    } else {
+      if (rotation == ROTATE_0) {
+        command(PSR,  {0xA3, 0x08});
+      } else {
+        command(PSR,  {0xAF, 0x08});
+      }
+    }
+    command(PWR,  {0x37, 0x00, 0x23, 0x23});
+    command(PFS,  {0x00});
+    command(BTST, {0xC7, 0xC7, 0x1D});
+    command(PLL,  {0x3C});
+    command(TSC,  {0x00});
+    command(CDI,  {0x37});
+    command(TCON, {0x22});
+    command(TRES, 4, dimensions);
+    command(PWS,  {0xAA});
+
+    sleep_ms(100);
+
+    command(0x50, {0x37});
+  }
+
+  void E2802::set_blocking(bool blocking) {
+    this->blocking = blocking;
+  }
+
+  void E2802::power_off() {
+    busy_wait();
+    command(POF); // turn off
+  }
+
+  void E2802::command(uint8_t reg, size_t len, const uint8_t *data) {
+    gpio_put(CS, 0);
+
+    gpio_put(DC, 0); // command mode
+    spi_write_blocking(spi, &reg, 1);
+
+    if(len > 0) {
+      gpio_put(DC, 1); // data mode
+      spi_write_blocking(spi, (const uint8_t*)data, len);
+    }
+
+    gpio_put(CS, 1);
+  }
+
+  void E2802::data(size_t len, const uint8_t *data) {
+    gpio_put(CS, 0);
+    gpio_put(DC, 1); // data mode
+    spi_write_blocking(spi, (const uint8_t*)data, len);
+    gpio_put(CS, 1);
+  }
+
+  void E2802::command(uint8_t reg, std::initializer_list<uint8_t> values) {
+    command(reg, values.size(), (uint8_t *)values.begin());
+  }
+
+  void E2802::update(PicoGraphics *graphics) {
+    if(graphics->pen_type != PicoGraphics::PEN_3BIT) return; // Incompatible buffer
+
+    if(blocking) {
+      busy_wait();
+    }
+
+    setup();
+
+    gpio_put(CS, 0);
+
+    uint8_t reg = DTM1;
+    gpio_put(DC, 0); // command mode
+    spi_write_blocking(spi, &reg, 1);
+
+    gpio_put(DC, 1); // data mode
+
+    // HACK: Output 48 rows of data since our buffer is 400px tall
+    // but the display has no offset configuration and H/V scan 
+    // are reversed.
+    // Any garbage data will do.
+    // 2px per byte, so we need width * 24 bytes
+    if(height == 400 && rotation == ROTATE_0) {
+      spi_write_blocking(spi, (uint8_t *)graphics->frame_buffer, width * 24);
+    }
+    graphics->frame_convert(PicoGraphics::PEN_P4, [this](void *buf, size_t length) {
+      if (length > 0) {
+        spi_write_blocking(spi, (const uint8_t*)buf, length);
+      }
+    });
+
+    gpio_put(CS, 1);
+
+    busy_wait();
+
+    command(PON); // turn on
+    busy_wait(200);
+
+    command(DRF); // start display refresh
+    busy_wait(200);
+
+    if(blocking) {
+      busy_wait(32 * 1000);
+
+      command(POF); // turn off
+    } else {
+      timeout = make_timeout_time_ms(32 * 1000);
+    }
+  }
+
+}
---
+++ drivers/e2802/e2802.hpp
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <initializer_list>
+
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/gpio.h"
+#include "common/pimoroni_common.hpp"
+#include "common/pimoroni_bus.hpp"
+#include "libraries/pico_graphics/pico_graphics.hpp"
+
+namespace pimoroni {
+
+  class E2802 : public DisplayDriver {
+    //--------------------------------------------------
+    // Variables
+    //--------------------------------------------------
+  private:
+    spi_inst_t *spi = PIMORONI_SPI_DEFAULT_INSTANCE;
+
+    // interface pins with our standard defaults where appropriate
+    uint CS     = SPI_BG_FRONT_CS;
+    uint DC     = 28; // 27;
+    uint SCK    = SPI_DEFAULT_SCK;
+    uint MOSI   = SPI_DEFAULT_MOSI;
+    uint BUSY   = PIN_UNUSED;
+    uint RESET  = 27; //25;
+
+    absolute_time_t timeout;
+
+    bool blocking = false;
+
+  public:
+    enum colour : uint8_t {
+      BLACK = 0,
+      WHITE = 1,
+      GREEN = 2,
+      BLUE = 3,
+      RED = 4,
+      YELLOW = 5,
+      ORANGE = 6,
+      CLEAN = 7
+    };
+
+    E2802(uint16_t width, uint16_t height) : E2802(width, height, ROTATE_0, {PIMORONI_SPI_DEFAULT_INSTANCE, SPI_BG_FRONT_CS, SPI_DEFAULT_SCK, SPI_DEFAULT_MOSI, PIN_UNUSED, 28, PIN_UNUSED}) {};
+
+    E2802(uint16_t width, uint16_t height, SPIPins pins, uint busy=PIN_UNUSED, uint reset=27) : E2802(width, height, ROTATE_0, pins, busy, reset) {};
+
+    E2802(uint16_t width, uint16_t height, Rotation rotation, SPIPins pins, uint busy=PIN_UNUSED, uint reset=27) :
+      DisplayDriver(width, height, rotation),
+      spi(pins.spi),
+      CS(pins.cs), DC(pins.dc), SCK(pins.sck), MOSI(pins.mosi), BUSY(busy), RESET(reset) {
+        init();
+      }
+
+
+    //--------------------------------------------------
+    // Methods
+    //--------------------------------------------------
+  public:
+    void busy_wait(uint minimum_wait_ms=0);
+    void reset();
+    void power_off();
+  
+    bool is_busy() override;
+    void update(PicoGraphics *graphics) override;
+
+    void set_blocking(bool blocking);
+
+  private:
+    void init();
+    void setup();
+    void command(uint8_t reg, size_t len, const uint8_t *data);
+    void command(uint8_t reg, std::initializer_list<uint8_t> values);
+    void command(uint8_t reg, const uint8_t data) {command(reg, 0, &data);};
+    void command(uint8_t reg) {command(reg, 0, nullptr);};
+    void data(size_t len, const uint8_t *data);
+  };
+
+}
