db2ab0012a14ef53c2d017cb18627e28d5c13226
AndrewCapon <andrew.capon@zen.co.uk>
2022-12-30 10:15:20 +0000

    Fix Typos

--- examples/ili9341_xpt2046/ili9341_demo_direct.cpp
+++ examples/ili9341_xpt2046/ili9341_demo_direct.cpp
@@ -14,15 +14,15 @@ const char* msg = "Hello ILI9341";
 
 Rotation rotation = ROTATE_270;
 
-#define ILI941_WIDTH (240)
-#define ILI941_HEIGHT (320)
+#define ILI9341_WIDTH (240)
+#define ILI9341_HEIGHT (320)
 // you might need to lower the baud rate
 // try 156'250'000 then 312'500'000 if you are having issues
-#define ILI941_BAUD_RATE (625'000'000)
+#define ILI9341_BAUD_RATE (625'000'000)
 
 // if you do not have a reset pin wired up change to
 // #define RESET_PIN PIN_UNUSED
-#define ILI941_RESET_PIN (21)
+#define ILI9341_RESET_PIN (21)
 
 // set to 1 if you have a XPT2046 touchscreen
 #define USE_TOUCHSCREEN 1
@@ -41,7 +41,7 @@ Rotation rotation = ROTATE_270;
   XPT2046	xpt2046(XPT2046_WIDTH, XPT2046_HEIGHT, (Rotation)((rotation+XPT2046_ROTATION_OFFSET)%360), touch_spi, XPT2046_IRQ);
 #endif
  
-ILI9341 ili9341(ILI941_WIDTH, ILI941_HEIGHT, rotation, false, get_spi_pins(BG_SPI_FRONT), ILI941_RESET_PIN, ILI941_BAUD_RATE);
+ILI9341 ili9341(ILI9341_WIDTH, ILI9341_HEIGHT, rotation, false, get_spi_pins(BG_SPI_FRONT), ILI9341_RESET_PIN, ILI9341_BAUD_RATE);
 PicoGraphics_PenRGB565_direct graphics(ili9341.width, ili9341.height, ili9341);
 
 int main() {
