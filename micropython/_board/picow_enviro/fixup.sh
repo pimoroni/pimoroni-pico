9f27f449a086b037770fd77307327f6704f3059d
ZodiusInfuser <christopher.parrott2@gmail.com>
2023-01-06 10:53:49 +0000

    Added debug echoes

--- micropython/_board/picow_enviro/fixup.sh
+++ micropython/_board/picow_enviro/fixup.sh
@@ -3,4 +3,11 @@ DST_DIR=$2
 
 echo "Applying wakeup_gpio.patch"
 cd "$DST_DIR/../../"
+echo "$DST_DIR/../../"
+echo "`pwd`"
+ls
+echo "$SRC_DIR"
+ls "$SRC_DIR"
+normalDir="`cd "$SRC_DIR";pwd`"
+echo "${normalDir}"
 git apply "$SRC_DIR/wakeup_gpio.patch"
