9ea196adb1d4a715e7ffec27f261679f026d6146
Phil Howard <phil@gadgetoid.com>
2023-01-27 13:01:40 +0000

    Badger2040W: Tidyup icons, crop to 52x52.

--- micropython/examples/badger2040w/launcher.py
+++ micropython/examples/badger2040w/launcher.py
@@ -92,7 +92,7 @@ def render():
         icon = f"{APP_DIR}/icon-{icon_label}.jpg"
         label = label.replace("_", " ")
         jpeg.open_file(icon)
-        jpeg.decode(x - 32, 24)
+        jpeg.decode(x - 26, 30)
         display.set_pen(0)
         w = display.measure_text(label, FONT_SIZE)
         display.text(label, int(x - (w / 2)), 16 + 80, WIDTH, FONT_SIZE)
