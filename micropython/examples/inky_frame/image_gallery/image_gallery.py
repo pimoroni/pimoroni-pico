0138980849ece8ab18651af34a12175695ac95e0
Hel Gibbons <hel@pimoroni.com>
2023-03-13 16:56:28 +0000

    update images, add new random image example

--- micropython/examples/inky_frame/image_gallery/image_gallery.py
+++ micropython/examples/inky_frame/image_gallery/image_gallery.py
@@ -1,17 +1,19 @@
-# An offline image gallery that switches between five jpg images
-# Copy them into the root of your Pico's flash using Thonny.
+"""
+An offline image gallery that switches between five jpg images.
 
-# If you want to use your own images they must be the screen dimensions (or smaller)
-# and saved as *non-progressive* jpgs.
+Copy images into the root of your Pico's flash using Thonny.
 
-# Make sure to uncomment the correct size for your display!
+If you want to use your own images they must be the screen dimensions
+(or smaller) and saved as *non-progressive* jpgs.
 
-from picographics import PicoGraphics, DISPLAY_INKY_FRAME as DISPLAY    # 5.7"
+Make sure to uncomment the correct size for your display!
+"""
+
+# from picographics import PicoGraphics, DISPLAY_INKY_FRAME as DISPLAY    # 5.7"
 # from picographics import PicoGraphics, DISPLAY_INKY_FRAME_4 as DISPLAY  # 4.0"
-# from picographics import PicoGraphics, DISPLAY_INKY_FRAME_7 as DISPLAY  # 7.3"
-from machine import Pin
+from picographics import PicoGraphics, DISPLAY_INKY_FRAME_7 as DISPLAY  # 7.3"
+import inky_frame
 import jpegdec
-from pimoroni import ShiftRegister
 
 # you can change your file names here
 IMAGE_A = "jwst1.jpg"
@@ -21,33 +23,10 @@ IMAGE_D = "jwst4.jpg"
 IMAGE_E = "jwst5.jpg"
 
 # set up the display
-graphics = PicoGraphics(DISPLAY)
-
-# Inky Frame uses a shift register to read the buttons
-SR_CLOCK = 8
-SR_LATCH = 9
-SR_OUT = 10
-
-sr = ShiftRegister(SR_CLOCK, SR_LATCH, SR_OUT)
-
-# set up the button LEDs
-button_a_led = Pin(11, Pin.OUT)
-button_b_led = Pin(12, Pin.OUT)
-button_c_led = Pin(13, Pin.OUT)
-button_d_led = Pin(14, Pin.OUT)
-button_e_led = Pin(15, Pin.OUT)
-
-# and the activity LED
-activity_led = Pin(6, Pin.OUT)
-
-# set up and enable vsys hold so we don't go to sleep
-HOLD_VSYS_EN_PIN = 2
-
-hold_vsys_en_pin = Pin(HOLD_VSYS_EN_PIN, Pin.OUT)
-hold_vsys_en_pin.value(True)
+display = PicoGraphics(DISPLAY)
 
 # Create a new JPEG decoder for our PicoGraphics
-j = jpegdec.JPEG(graphics)
+j = jpegdec.JPEG(display)
 
 
 def display_image(filename):
@@ -59,51 +38,38 @@ def display_image(filename):
     j.decode(0, 0, jpegdec.JPEG_SCALE_FULL)
 
     # Display the result
-    graphics.update()
+    display.update()
 
 
-# setup
-activity_led.on()
-# update the image on Inky every time it's powered up
-# comment these lines out if running on battery power
-# button_a_led.on()
-# display_image(IMAGE_A)
+print('Press a button to display an image!')
 
 while True:
-    button_a_led.off()
-    button_b_led.off()
-    button_c_led.off()
-    button_d_led.off()
-    button_e_led.off()
+    inky_frame.button_a.led_off()
+    inky_frame.button_b.led_off()
+    inky_frame.button_c.led_off()
+    inky_frame.button_d.led_off()
+    inky_frame.button_e.led_off()
 
-    # read the shift register
-    # we can tell which button has been pressed by checking if a specific bit is 0 or 1
-    result = sr.read()
-    button_a = sr[7]
-    button_b = sr[6]
-    button_c = sr[5]
-    button_d = sr[4]
-    button_e = sr[3]
-
-    # light up the activity LED when Inky is awake
-    activity_led.on()
-
-    if button_a == 1:
-        button_a_led.on()
+    if inky_frame.button_a.read():
+        print('Refreshing image A.')
+        inky_frame.button_a.led_on()
         display_image(IMAGE_A)
-    elif button_b == 1:
-        button_b_led.on()
+    elif inky_frame.button_b.read():
+        print('Refreshing image B.')
+        inky_frame.button_b.led_on()
         display_image(IMAGE_B)
-    elif button_c == 1:
-        button_c_led.on()
+    elif inky_frame.button_c.read():
+        print('Refreshing image C.')
+        inky_frame.button_c.led_on()
         display_image(IMAGE_C)
-    elif button_d == 1:
-        button_d_led.on()
+    elif inky_frame.button_d.read():
+        print('Refreshing image D.')
+        inky_frame.button_d.led_on()
         display_image(IMAGE_D)
-    elif button_e == 1:
-        button_e_led.on()
+    elif inky_frame.button_e.read():
+        print('Refreshing image E.')
+        inky_frame.button_e.led_on()
         display_image(IMAGE_E)
 
     # Go to sleep if on battery power
-    activity_led.off()
-    hold_vsys_en_pin.init(Pin.IN)
+    inky_frame.turn_off()
--- micropython/examples/inky_frame/image_gallery/image_gallery_sd.py
+++ micropython/examples/inky_frame/image_gallery/image_gallery_sd.py
@@ -1,20 +1,22 @@
-# An offline image gallery that switches between five jpg images
-# on your SD card (copy them across by plugging your SD into a computer).
+"""
+An offline image gallery that switches between five jpg images on your SD card.
 
-# If you want to use your own images they must be the screen dimensions (or smaller)
-# and saved as *non-progressive* jpgs
+Copy images to the root of your SD card by plugging it into a computer.
 
-# Make sure to uncomment the correct size for your display!
+If you want to use your own images they must be the screen dimensions
+(or smaller) and saved as *non-progressive* jpgs.
 
-from picographics import PicoGraphics, DISPLAY_INKY_FRAME as DISPLAY    # 5.7"
+Make sure to uncomment the correct size for your display!
+"""
+
+# from picographics import PicoGraphics, DISPLAY_INKY_FRAME as DISPLAY    # 5.7"
 # from picographics import PicoGraphics, DISPLAY_INKY_FRAME_4 as DISPLAY  # 4.0"
-# from picographics import PicoGraphics, DISPLAY_INKY_FRAME_7 as DISPLAY  # 7.3"
-from pimoroni import ShiftRegister
+from picographics import PicoGraphics, DISPLAY_INKY_FRAME_7 as DISPLAY  # 7.3"
 from machine import Pin, SPI
 import jpegdec
 import sdcard
-import uos
-
+import os
+import inky_frame
 
 # you can change your file names here
 IMAGE_A = "sd/jwst1.jpg"
@@ -26,33 +28,10 @@ IMAGE_E = "sd/jwst5.jpg"
 # set up the display
 graphics = PicoGraphics(DISPLAY)
 
-# Inky Frame uses a shift register to read the buttons
-SR_CLOCK = 8
-SR_LATCH = 9
-SR_OUT = 10
-
-sr = ShiftRegister(SR_CLOCK, SR_LATCH, SR_OUT)
-
-# set up the button LEDs
-button_a_led = Pin(11, Pin.OUT)
-button_b_led = Pin(12, Pin.OUT)
-button_c_led = Pin(13, Pin.OUT)
-button_d_led = Pin(14, Pin.OUT)
-button_e_led = Pin(15, Pin.OUT)
-
-# and the activity LED
-activity_led = Pin(6, Pin.OUT)
-
 # set up the SD card
 sd_spi = SPI(0, sck=Pin(18, Pin.OUT), mosi=Pin(19, Pin.OUT), miso=Pin(16, Pin.OUT))
 sd = sdcard.SDCard(sd_spi, Pin(22))
-uos.mount(sd, "/sd")
-
-# set up and enable vsys hold so we don't go to sleep
-HOLD_VSYS_EN_PIN = 2
-
-hold_vsys_en_pin = Pin(HOLD_VSYS_EN_PIN, Pin.OUT)
-hold_vsys_en_pin.value(True)
+os.mount(sd, "/sd")
 
 # Create a new JPEG decoder for our PicoGraphics
 j = jpegdec.JPEG(graphics)
@@ -71,47 +50,35 @@ def display_image(filename):
 
 
 # setup
-activity_led.on()
-# update the image on Inky every time it's powered up
-# comment these lines out if running on battery power
-# button_a_led.on()
-# display_image(IMAGE_A)
+print('Press a button to display an image!')
 
 while True:
-    button_a_led.off()
-    button_b_led.off()
-    button_c_led.off()
-    button_d_led.off()
-    button_e_led.off()
+    inky_frame.button_a.led_off()
+    inky_frame.button_b.led_off()
+    inky_frame.button_c.led_off()
+    inky_frame.button_d.led_off()
+    inky_frame.button_e.led_off()
 
-    # read the shift register
-    # we can tell which button has been pressed by checking if a specific bit is 0 or 1
-    result = sr.read()
-    button_a = sr[7]
-    button_b = sr[6]
-    button_c = sr[5]
-    button_d = sr[4]
-    button_e = sr[3]
-
-    # light up the activity LED when Inky is awake
-    activity_led.on()
-
-    if button_a == 1:
-        button_a_led.on()
+    if inky_frame.button_a.read():
+        print('Refreshing image A.')
+        inky_frame.button_a.led_on()
         display_image(IMAGE_A)
-    elif button_b == 1:
-        button_b_led.on()
+    elif inky_frame.button_b.read():
+        print('Refreshing image B.')
+        inky_frame.button_b.led_on()
         display_image(IMAGE_B)
-    elif button_c == 1:
-        button_c_led.on()
+    elif inky_frame.button_c.read():
+        print('Refreshing image C.')
+        inky_frame.button_c.led_on()
         display_image(IMAGE_C)
-    elif button_d == 1:
-        button_d_led.on()
+    elif inky_frame.button_d.read():
+        print('Refreshing image D.')
+        inky_frame.button_d.led_on()
         display_image(IMAGE_D)
-    elif button_e == 1:
-        button_e_led.on()
+    elif inky_frame.button_e.read():
+        print('Refreshing image E.')
+        inky_frame.button_e.led_on()
         display_image(IMAGE_E)
 
-    # go to sleep if on battery power
-    activity_led.off()
-    hold_vsys_en_pin.init(Pin.IN)
+    # Go to sleep if on battery power
+    inky_frame.turn_off()
