cce02feabd916c19817d811ae9432f810032a6b9
Phil Howard <phil@pimoroni.com>
2023-03-28 21:46:04 +0100

    CI: Set MICROPY_BOARD_DIR in CI workflow.
    Move out of the USER_C_MODULES .cmake file.

--- micropython/board/PIMORONI_TUFTY2040/mpconfigboard.cmake
+++ micropython/board/PIMORONI_TUFTY2040/mpconfigboard.cmake
@@ -1,1 +1,5 @@
 # cmake file for Pimoroni Tufty 2040
+set(PICO_BOARD "pimoroni_tufty2040")
+
+# Allow Pico SDK to locate "pimoroni_tufty2040.h" in this directory.
+list(APPEND PICO_BOARD_HEADER_DIRS "${CMAKE_CURRENT_LIST_DIR}")
