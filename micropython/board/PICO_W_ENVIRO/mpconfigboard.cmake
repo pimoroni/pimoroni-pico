3bdb27458fa8b2151333b91e6a213a3e5ae46b7f
Phil Howard <phil@pimoroni.com>
2023-03-28 22:12:00 +0100

    CI: Don't monkey with manifest.py paths.

--- micropython/board/PICO_W_ENVIRO/mpconfigboard.cmake
+++ micropython/board/PICO_W_ENVIRO/mpconfigboard.cmake
@@ -3,6 +3,3 @@ set(MICROPY_BOARD PICO_W)
 
 set(MICROPY_PY_LWIP ON)
 set(MICROPY_PY_NETWORK_CYW43 ON)
-
-# Board specific version of the frozen manifest
-set(MICROPY_FROZEN_MANIFEST ${CMAKE_SOURCE_DIR}/boards/PICO_W_ENVIRO/manifest.py)
