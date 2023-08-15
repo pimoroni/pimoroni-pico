817b9c2f41aad9a67fec3d2a9387b295eaa37acd
Phil Howard <phil@gadgetoid.com>
2023-08-15 14:25:55 +0100

    PicoVector: Vector anti-aliasing support.

--- libraries/pico_vector/pico_vector.hpp
+++ libraries/pico_vector/pico_vector.hpp
@@ -7,21 +7,39 @@ namespace pimoroni {
         private:
             PicoGraphics *graphics;
             alright_fonts::text_metrics_t text_metrics;
+            const uint8_t alpha_map[4] {0, 128, 192, 255};
 
         public:
             PicoVector(PicoGraphics *graphics, void *mem = nullptr) : graphics(graphics) {
                 pretty_poly::init(mem);
 
-                set_options([this](const pretty_poly::tile_t &tile) -> void {
-                    for(auto y = 0; y < tile.bounds.h; y++) {
-                        for(auto x = 0; x < tile.bounds.w; x++) {
-                            uint8_t alpha = tile.get_value(x, y);
-                            if (alpha > 0) {
-                                this->graphics->pixel({x + tile.bounds.x, y + tile.bounds.y});
+
+                if(graphics->supports_alpha_blend()) {
+                    set_options([this](const pretty_poly::tile_t &tile) -> void {
+                        for(auto y = 0; y < tile.bounds.h; y++) {
+                            for(auto x = 0; x < tile.bounds.w; x++) {
+                                uint8_t alpha = tile.get_value(x, y);
+                                if (alpha >= 4) {
+                                    this->graphics->pixel({x + tile.bounds.x, y + tile.bounds.y});
+                                } else if (alpha > 0) {
+                                    alpha = alpha_map[alpha];
+                                    this->graphics->set_pixel_alpha({x + tile.bounds.x, y + tile.bounds.y}, alpha);
+                                }
                             }
                         }
-                    }
-                }, pretty_poly::X4, {0, 0, graphics->bounds.w, graphics->bounds.h});
+                    }, pretty_poly::X4, {0, 0, graphics->bounds.w, graphics->bounds.h});
+                } else {
+                    set_options([this](const pretty_poly::tile_t &tile) -> void {
+                        for(auto y = 0; y < tile.bounds.h; y++) {
+                            for(auto x = 0; x < tile.bounds.w; x++) {
+                                uint8_t alpha = tile.get_value(x, y);
+                                if (alpha > 0) {
+                                    this->graphics->pixel({x + tile.bounds.x, y + tile.bounds.y});
+                                }
+                            }
+                        }
+                    }, pretty_poly::NONE, {0, 0, graphics->bounds.w, graphics->bounds.h});
+                }
             };
 
             bool set_font(std::string_view font_path, unsigned int font_size) {
