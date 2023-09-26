211f8080cbf93d322ebdbbdfb8e3b8de5e4ec6d7
Phil Howard <phil@gadgetoid.com>
2023-09-26 13:37:55 +0100

    PicoVector: Rewrite around new C pretty-poly.h.

--- libraries/pico_vector/pico_vector.cmake
+++ libraries/pico_vector/pico_vector.cmake
@@ -1,9 +1,12 @@
+if(NOT TARGET pico_graphics)
+    include(${CMAKE_CURRENT_LIST_DIR}/../pico_graphics/pico_graphics.cmake)
+endif()
+
 add_library(pico_vector 
     ${CMAKE_CURRENT_LIST_DIR}/pico_vector.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/pretty_poly.cpp
     ${CMAKE_CURRENT_LIST_DIR}/alright_fonts.cpp
 )
 
 target_include_directories(pico_vector INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
-target_link_libraries(pico_vector pico_stdlib hardware_interp)
+target_link_libraries(pico_vector pico_graphics pico_stdlib hardware_interp)
