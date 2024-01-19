1a6db1fa8be0bda75849188236c3900ea651e95e
Phil Howard <github@gadgetoid.com>
2024-01-19 10:50:34 +0000

    Experimental: builds with dual FAT MSC support.

--- micropython/board/RPI_PICO/mpconfigboard.h
+++ micropython/board/RPI_PICO/mpconfigboard.h
@@ -1,3 +1,10 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+
+#define MICROPY_HW_USB_MSC (1)
+#define MICROPY_HW_USB_VID (0x1FFB)
+#define MICROPY_HW_USB_PID (0x2043)
+#define MICROPY_HW_USB_DESC_STR_MAX (40)
+#define MICROPY_HW_USB_MANUFACTURER_STRING "Pimoroni"
+#define MICROPY_HW_USB_PRODUCT_FS_STRING MICROPY_HW_BOARD_NAME " MicroPython"
--- micropython/board/RPI_PICO_W/mpconfigboard.h
+++ micropython/board/RPI_PICO_W/mpconfigboard.h
@@ -22,3 +22,10 @@
 
 int mp_hal_is_pin_reserved(int n);
 #define MICROPY_HW_PIN_RESERVED(i) mp_hal_is_pin_reserved(i)
+
+#define MICROPY_HW_USB_MSC (1)
+#define MICROPY_HW_USB_VID (0x1FFB)
+#define MICROPY_HW_USB_PID (0x2043)
+#define MICROPY_HW_USB_DESC_STR_MAX (40)
+#define MICROPY_HW_USB_MANUFACTURER_STRING "Pimoroni"
+#define MICROPY_HW_USB_PRODUCT_FS_STRING MICROPY_HW_BOARD_NAME " MicroPython"
