392890e99b26c986786e6a89437e1fa96dbbda76
Phil Howard <github@gadgetoid.com>
2024-03-25 10:26:21 +0000

    RP_PICO_USB: New USB-enabled version of the stock Pico firmware.
    :warning: Warning: flash/firmware split is now 1Mb/1Mb to accomodate USB libs.
    
    BACK UP YOUR PICO before flashing this build. It *will* trash your filesystem.
    
    * Add new RP_PICO_USB board
    * Use commit 932f76c6ba64c5a3e68de3324556d9979f09303b for https://github.com/micropython/micropython/commit/932f76c6ba64c5a3e68de3324556d9979f09303b

---
+++ micropython/board/RPI_PICO_USB/mpconfigboard.h
@@ -0,0 +1,4 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (1024 * 1024)
+#define MICROPY_HW_ENABLE_USB_RUNTIME_DEVICE    1
---
+++ micropython/modules/micropython-pico_usb.cmake
@@ -0,0 +1,14 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../../)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
+
+include(micropython-common)
+include(pico_wireless/micropython)
+
+# C++ Magic Memory
+include(cppmem/micropython)
