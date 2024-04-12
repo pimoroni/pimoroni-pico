9aaed0af037ee5735499320e53047204d714542a
Phil Howard <phil@gadgetoid.com>
2024-04-12 14:02:32 +0100

    CI: TEST: Add 1023/1024 split Pico build.

---
+++ micropython/board/PIMORONI_PICO/mpconfigboard.h
@@ -0,0 +1,3 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico 1024"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (1024 * 1024)
---
+++ micropython/modules/micropython-pimoroni_pico.cmake
@@ -0,0 +1,16 @@
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../../)
+
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_CXX_STANDARD 17)
+
+include(micropython-common)
+include(pico_wireless/micropython)
+
+enable_ulab()
+
+# C++ Magic Memory
+include(cppmem/micropython)
