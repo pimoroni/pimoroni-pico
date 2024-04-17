8a6dcb7684ec67b3a46b4ec2b76a1855b6defa1e
Phil Howard <phil@gadgetoid.com>
2024-04-17 16:17:54 +0100

    PicoVector: Suppress errors.
    Ignore sign compare and narrowing conversion errors in pretty-poly.h.

--- libraries/pico_vector/pico_vector.cmake
+++ libraries/pico_vector/pico_vector.cmake
@@ -10,3 +10,9 @@ add_library(pico_vector
 target_include_directories(pico_vector INTERFACE ${CMAKE_CURRENT_LIST_DIR})
 
 target_link_libraries(pico_vector pico_graphics pico_stdlib hardware_interp)
+
+set_source_files_properties(
+    ${CMAKE_CURRENT_LIST_DIR}/pico_vector.cpp
+    PROPERTIES COMPILE_FLAGS
+    "-Wno-narrowing"
+)
