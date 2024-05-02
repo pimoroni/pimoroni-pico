87404ee781f78f4e527aec55260db56861aa582d
Phil Howard <phil@gadgetoid.com>
2024-05-02 19:57:46 +0100

    Pint: Trim some cruft from the build so it doesn't esplode.

--- micropython/modules/micropython-pico_pint.cmake
+++ micropython/modules/micropython-pico_pint.cmake
@@ -7,7 +7,33 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/../../")
 set(CMAKE_C_STANDARD 11)
 set(CMAKE_CXX_STANDARD 17)
 
-include(micropython-common)
+# Essential
+include(pimoroni_i2c/micropython)
+include(pimoroni_bus/micropython)
+
+# Sensors & Breakouts
+include(micropython-common-breakouts)
+
+# LEDs & Matrices
+include(plasma/micropython)
+include(hub75/micropython)
+
+# Servos & Motors
+include(pwm/micropython)
+include(servo/micropython)
+include(encoder/micropython)
+include(motor/micropython)
+
+# Utility
+include(adcfft/micropython)
+
+# RTC (Badger 2040W, Enviro)
+if(PICO_BOARD STREQUAL "pico_w")
+    include(pcf85063a/micropython)
+endif()
+
+include(modules_py/modules_py)
+
 include(pint/micropython)
 
 # C++ Magic Memory
