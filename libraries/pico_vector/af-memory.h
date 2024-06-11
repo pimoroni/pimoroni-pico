9971c7c4f4183c2c2437605c760708eee42ffa98
Phil Howard <phil@gadgetoid.com>
2024-06-11 14:39:06 +0100

    PicoVector: C++ basic bringup.

--- libraries/pico_vector/af-memory.h
+++ libraries/pico_vector/af-memory.h
@@ -1,6 +1,12 @@
+#include <stddef.h>
+
+#ifdef __cplusplus
 extern "C" {
+#endif
     void *af_malloc(size_t size);
     void *af_realloc(void *p, size_t size);
     void af_free(void *p);
     void af_debug(const char *fmt, ...);
+#ifdef __cplusplus
 }
+#endif
