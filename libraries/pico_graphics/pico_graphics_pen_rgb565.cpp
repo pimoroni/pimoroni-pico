3de60d1ddc0ca812d593302c6bd1ef94ff2bbc65
Phil Howard <github@gadgetoid.com>
2024-10-07 14:36:06 +0100

    PicoGraphics: Layers.
    Add preliminary support for multiple layered drawing surfaces.
    
    Allows, for example, static content to be loaded into one layer and
    remain unmodified while the above layer contains animations.
    
    Particularly useful for drawing PNG or JPEG UI elements which are
    then overdrawn with text or animated elements, without paying the
    cost of loading/decoding every frame.

--- libraries/pico_graphics/pico_graphics_pen_rgb565.cpp
+++ libraries/pico_graphics/pico_graphics_pen_rgb565.cpp
@@ -1,8 +1,8 @@
 #include "pico_graphics.hpp"
 
 namespace pimoroni {
-    PicoGraphics_PenRGB565::PicoGraphics_PenRGB565(uint16_t width, uint16_t height, void *frame_buffer)
-    : PicoGraphics(width, height, frame_buffer) {
+    PicoGraphics_PenRGB565::PicoGraphics_PenRGB565(uint16_t width, uint16_t height, void *frame_buffer, uint16_t layers)
+    : PicoGraphics(width, height, layers, frame_buffer) {
         this->pen_type = PEN_RGB565;
         if(this->frame_buffer == nullptr) {
             this->frame_buffer = (void *)(new uint8_t[buffer_size(width, height)]);
@@ -23,11 +23,15 @@ namespace pimoroni {
     }
     void PicoGraphics_PenRGB565::set_pixel(const Point &p) {
         uint16_t *buf = (uint16_t *)frame_buffer;
+        // We can't use buffer_size because our pointer is uint16_t
+        buf += this->bounds.w * this->bounds.h * layer;
         buf[p.y * bounds.w + p.x] = color;
     }
     void PicoGraphics_PenRGB565::set_pixel_span(const Point &p, uint l) {
         // pointer to byte in framebuffer that contains this pixel
         uint16_t *buf = (uint16_t *)frame_buffer;
+        // We can't use buffer_size because our pointer is uint16_t
+        buf += this->bounds.w * this->bounds.h * layer;
         buf = &buf[p.y * bounds.w + p.x];
 
         while(l--) {
@@ -35,26 +39,50 @@ namespace pimoroni {
         }
     }
 
+    void PicoGraphics_PenRGB565::frame_convert(PenType type, conversion_callback_func callback) {
+        if(type == PEN_RGB565) {
+            // Treat our void* frame_buffer as uint8_t
+            uint16_t *src = (uint16_t *)frame_buffer;
 
-     void PicoGraphics_PenRGB565::sprite(void* data, const Point &sprite, const Point &dest, const int scale, const int transparent) {
-         //int sprite_x = (sprite & 0x0f) << 3;
-         //int sprite_y = (sprite & 0xf0) >> 1;
-         Point s {
-             sprite.x << 3,
-             sprite.y << 3
-         };
-         RGB565 *ptr = (RGB565 *)data;
-         Point o = {0, 0};
-         for(o.y = 0; o.y < 8 * scale; o.y++) {
-             Point so = {
-                 0,
-                 o.y / scale
-             };
-             for(o.x = 0; o.x < 8 * scale; o.x++) {
-                 so.x = o.x / scale;
-                 color = ptr[(s.y + so.y) * 128 + (s.x + so.x)];
-                 if(color != transparent) pixel(dest + o);
-             }
-         }
-     }
+            if(layers > 1) {
+                // Assume only two layers for now
+                uint16_t *src_layer2 = src;
+
+                // We can't use buffer_size because our pointer is uint16_t
+                src_layer2 += this->bounds.w * this->bounds.h * layer;
+
+                frame_convert_rgb565(callback, [&]() {
+                    RGB565 c1 = *src++;
+                    RGB565 c2 = *src_layer2++;
+                    return c2 ? c2 : c1;
+                });
+            } else {
+                frame_convert_rgb565(callback, [&]() {
+                    return *src++;
+                });
+            }
+        }
+    }
+
+    void PicoGraphics_PenRGB565::sprite(void* data, const Point &sprite, const Point &dest, const int scale, const int transparent) {
+        //int sprite_x = (sprite & 0x0f) << 3;
+        //int sprite_y = (sprite & 0xf0) >> 1;
+        Point s {
+            sprite.x << 3,
+            sprite.y << 3
+        };
+        RGB565 *ptr = (RGB565 *)data;
+        Point o = {0, 0};
+        for(o.y = 0; o.y < 8 * scale; o.y++) {
+            Point so = {
+                0,
+                o.y / scale
+            };
+            for(o.x = 0; o.x < 8 * scale; o.x++) {
+                so.x = o.x / scale;
+                color = ptr[(s.y + so.y) * 128 + (s.x + so.x)];
+                if(color != transparent) pixel(dest + o);
+            }
+        }
+    }
 }
