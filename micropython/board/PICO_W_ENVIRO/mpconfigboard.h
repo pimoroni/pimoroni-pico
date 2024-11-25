724505f6d5fb4fa2f2a21a57758da187714fc67d
Phil Howard <github@gadgetoid.com>
2024-11-25 14:37:28 +0000

    MicroPython: Switch board configs to enable_cyw43.h.

--- micropython/board/PICO_W_ENVIRO/mpconfigboard.h
+++ micropython/board/PICO_W_ENVIRO/mpconfigboard.h
@@ -9,15 +9,8 @@
 #define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT     "EnviroW"
 
 // CYW43 driver configuration.
-#define CYW43_USE_SPI (1)
-#define CYW43_LWIP (1)
-#define CYW43_GPIO (1)
-#define CYW43_SPI_PIO (1)
+#include "enable_cyw43.h"
 
 // For debugging mbedtls - also set
 // Debug level (0-4) 1=warning, 2=info, 3=debug, 4=verbose
 // #define MODUSSL_MBEDTLS_DEBUG_LEVEL 1
-
-#define MICROPY_HW_PIN_EXT_COUNT    CYW43_WL_GPIO_COUNT
-
-#define MICROPY_HW_PIN_RESERVED(i) ((i) == CYW43_PIN_WL_HOST_WAKE || (i) == CYW43_PIN_WL_REG_ON)
