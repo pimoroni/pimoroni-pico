1488a0b7964ac0ed6758d3b305845471ec7108df
Phil Howard <github@gadgetoid.com>
2025-01-23 14:55:13 +0000

    PNG/JPEGDEC: Remove finalizer.
    Remove the finalizer from PNGDEC and JPEGDEC, since it was both redundant
    and actively harmful.
    
    The finalizer was just calling "close" which is unecessary since the file
    is closed automatically after opening/rendering and not left open.
    
    If a file did not exist and "open_file" bailed with ENOENT then it would
    be left in an unknown state, and the finalizer calling "close" would
    cause a hardlock.

--- micropython/modules/pngdec/pngdec.h
+++ micropython/modules/pngdec/pngdec.h
@@ -4,7 +4,6 @@
 extern const mp_obj_type_t PNG_type;
 
 extern mp_obj_t _PNG_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args);
-extern mp_obj_t _PNG_del(mp_obj_t self_in);
 extern mp_obj_t _PNG_openRAM(mp_obj_t self_in, mp_obj_t buffer);
 extern mp_obj_t _PNG_openFILE(mp_obj_t self_in, mp_obj_t filename);
 extern mp_obj_t _PNG_decode(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
