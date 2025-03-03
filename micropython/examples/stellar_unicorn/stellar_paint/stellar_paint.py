27cb270bc075e42d37ca6cb1a36b77e4401da5bd
thirdr <ryan@pimoroni.com>
2025-03-03 09:40:42 +0000

    compatibility changes for use with microdot 2.x

--- micropython/examples/stellar_unicorn/stellar_paint/stellar_paint.py
+++ micropython/examples/stellar_unicorn/stellar_paint/stellar_paint.py
@@ -1,6 +1,5 @@
 import os
-from microdot_asyncio import Microdot, send_file
-from microdot_asyncio_websocket import with_websocket
+from microdot import Microdot, send_file, websocket
 from phew import connect_to_wifi
 from stellar import StellarUnicorn
 from picographics import PicoGraphics, DISPLAY_STELLAR_UNICORN as DISPLAY
@@ -75,7 +74,7 @@ def flood_fill(x, y, r, g, b):
 
 
 @server.route('/paint')
-@with_websocket
+@websocket.with_websocket
 async def echo(request, ws):
     while True:
         data = await ws.receive()
